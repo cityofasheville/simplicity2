7d6d9c3a1572ba383c65ccb2f27c64b7b4bef51b
ejaxon <eric@deepweave.com>
2016-08-31 13:34:35 -0400

    missed one

--- internals/templates/selectors.js
+++ internals/templates/selectors.js
@@ -4,7 +4,7 @@ const selectLocationState = () => {
   let prevRoutingStateJS;
 
   return (state) => {
-    const routingState = state.get('route'); // or state.route
+    const routingState = state.route; // or state.route
 
     if (!routingState.equals(prevRoutingState)) {
       prevRoutingState = routingState;

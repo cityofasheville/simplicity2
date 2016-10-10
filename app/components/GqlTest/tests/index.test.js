8239b3679009441c771f7dbe9a0529ebd5161e23
ejaxon <eric@deepweave.com>
2016-10-10 15:55:38 -0400

    Added a sample component using GraphQL data and a sample use in the topic container page

---
+++ app/components/GqlTest/tests/index.test.js
@@ -0,0 +1,11 @@
+// import GqlTest from '../gqlTest';
+
+import expect from 'expect';
+// import { shallow } from 'enzyme';
+// import React from 'react';
+
+describe('<GqlTest />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});

e78b604ada7662ea6f1d8dbb3a669fe8a7a2c890
ejaxon <eric@deepweave.com>
2016-11-21 13:46:44 -0500

    Created a bar chart skeleton component

---
+++ app/components/BarChart/barChart.js
@@ -0,0 +1,19 @@
+/**
+*
+* BarChart
+*
+*/
+
+import React from 'react';
+
+
+class BarChart extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default BarChart;
---
+++ app/components/BarChart/tests/index.test.js
@@ -0,0 +1,11 @@
+// import BarChart from '../barChart';
+
+import expect from 'expect';
+// import { shallow } from 'enzyme';
+// import React from 'react';
+
+describe('<BarChart />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});

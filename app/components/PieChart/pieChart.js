b3809b545dedc63584cfaa1bdc18bdfdfc738382
ejaxon <eric@deepweave.com>
2016-11-21 15:05:58 -0500

    Adding a pie chart component - just the skeleton

---
+++ app/components/PieChart/pieChart.js
@@ -0,0 +1,19 @@
+/**
+*
+* PieChart
+*
+*/
+
+import React from 'react';
+
+
+class PieChart extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default PieChart;

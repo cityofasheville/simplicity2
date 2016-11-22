6988db70216188d37604fb380619e3c8d092fa37
ejaxon <eric@deepweave.com>
2016-11-22 07:36:32 -0500

    Just a placeholder for statistics computations

---
+++ app/modules/Statistics/tests/index.test.js
@@ -0,0 +1,11 @@
+// import Statistics from '../index';
+
+import expect from 'expect';
+// import { shallow } from 'enzyme';
+// import React from 'react';
+
+describe('<Statistics />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});

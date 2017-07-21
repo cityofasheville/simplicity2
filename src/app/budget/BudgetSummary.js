3e2c1393e81f3b931729aa555114eb1c4ea7994c
Eric Jackson <ejackson@ashevillenc.gov>
2017-07-21 09:16:02 -0400

    Update "Proposed" to "Adopted"

--- src/app/budget/BudgetSummary.js
+++ src/app/budget/BudgetSummary.js
@@ -11,7 +11,7 @@ import { IM_COIN_DOLLAR } from '../../shared/iconConstants';
 
 const BudgetSummary = props => (
   <div>
-    <PageHeader h1="Proposed Budget 2017-2018" externalLinkText="Full budget document" externalLink="http://www.ashevillenc.gov/civicax/filebank/blobdload.aspx?blobid=27587" dataLinkPath="/budget/data" icon={<Icon path={IM_COIN_DOLLAR} size={45} />}>
+    <PageHeader h1="Adopted Budget 2017-2018" externalLinkText="Full budget document" externalLink="http://www.ashevillenc.gov/civicax/filebank/blobdload.aspx?blobid=27587" dataLinkPath="/budget/data" icon={<Icon path={IM_COIN_DOLLAR} size={45} />}>
       <ButtonGroup>
         <LinkButton pathname="/budget" query={{ entity: props.location.query.entity, id: props.location.query.id, label: props.location.query.label, hideNavbar: props.location.query.hideNavbar }} active positionInGroup="left">Summary</LinkButton>
         <LinkButton pathname="/budget/details" query={{ entity: props.location.query.entity, id: props.location.query.id, label: props.location.query.label, mode: props.location.query.mode || 'expenditures', hideNavbar: props.location.query.hideNavbar }} positionInGroup="right">Details</LinkButton>

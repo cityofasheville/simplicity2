fcefb869800845e937489665337bf395943052ad
fruiz7 <frances.ruiz@gmail.com>
2017-08-22 17:02:03 -0400

    added icons, still need to finalize colors

--- src/app/capital_projects/ProjectsTable.js
+++ src/app/capital_projects/ProjectsTable.js
@@ -4,7 +4,7 @@ import ReactTable from 'react-table';
 import { RadioGroup, Radio } from 'react-radio-group';
 import ProjectDetails from './ProjectDetails';
 import Icon from '../../shared/Icon';
-import { IM_CIRCLE2 } from '../../shared/iconConstants.js';
+import { IM_SHEILD3, IM_TREE3, IM_HOME2, IM_BUS } from '../../shared/iconConstants.js';
 
 const getStageNumber = (stage) => {
   switch (stage) {
@@ -36,10 +36,35 @@ const phaseColor = (phaseNumber) => {
   }
 };
 
+const getIcon = (category, isExpanded) => {
+  switch (category) {
+    case 'Bond - Parks Program':
+      return <Icon path={IM_TREE3} size={25} color={isExpanded ? "fff" : "#4077a5"} />;
+    case 'CIP - Parks & Recreation':
+      return <Icon path={IM_TREE3} size={25} color={isExpanded ? "fff" : "#4077a5"}/>;
+    case 'Bond - Transportation Program':
+      return <Icon path={IM_BUS} size={25} color={isExpanded ? "fff" : "#4077a5"} />
+    case 'CIP - Transportation & Infrastructure':
+      return <Icon path={IM_BUS} size={25} color={isExpanded ? "fff" : "#4077a5"} />
+    case 'Bond - Housing Program':
+      return <Icon path={IM_HOME2} size={25} color={isExpanded ? "fff" : "#4077a5"} />
+    case 'CIP - Affordable Housing':
+      return <Icon path={IM_HOME2} size={25} color={isExpanded ? "fff" : "#4077a5"} />
+    default:
+      return <svg xmlns="http://www.w3.org/2000/svg" height="25px" version="1.1" viewBox="0 0 16 16" width="25px"><g fill="none" fillRule="evenodd" id="Icons with numbers" stroke="none" stroke-width="1"><g fill={isExpanded ? "#fff" : "#4077a5"} id="Group" transform="translate(-528.000000, -576.000000)"><path d="M536,592 C531.581722,592 528,588.418278 528,584 C528,579.581722 531.581722,576 536,576 C540.418278,576 544,579.581722 544,584 C544,588.418278 540.418278,592 536,592 Z M541,586 C542.10457,586 543,585.10457 543,584 C543,582.89543 542.10457,582 541,582 C539.89543,582 539,582.89543 539,584 C539,585.10457 539.89543,586 541,586 Z M531,586 C532.10457,586 533,585.10457 533,584 C533,582.89543 532.10457,582 531,582 C529.89543,582 529,582.89543 529,584 C529,585.10457 529.89543,586 531,586 Z M536,586 C537.10457,586 538,585.10457 538,584 C538,582.89543 537.10457,582 536,582 C534.89543,582 534,582.89543 534,584 C534,585.10457 534.89543,586 536,586 Z M536,586" id="Oval 12 copy"/></g></g></svg>;
+  }
+}
+
 const dataColumns = [
   {
     Header: 'Project',
     accessor: 'Display Name',
+    Cell: (row) => (
+      <span>
+        <span>{getIcon(row.original.Category, row.isExpanded)}</span>
+        <span style={{ marginLeft: '5px' }}>{row.value}</span>
+      </span>
+    ),
   },
   {
     Header: (<div>Zip<br />code</div>),

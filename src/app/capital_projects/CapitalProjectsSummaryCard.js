472b57df165d98b762a4dc59e85586db6bab4fb4
fruiz7 <frances.ruiz@gmail.com>
2017-08-29 13:34:36 -0400

    closes #175 DASH-58 see if can use our tile service for CIP maps

--- src/app/capital_projects/CapitalProjectsSummaryCard.js
+++ src/app/capital_projects/CapitalProjectsSummaryCard.js
@@ -7,9 +7,9 @@ import { testProjectData, getFundsAllocatedAndExpended } from './cip_utilities'
 
 const CapitalProjectsSummaryCard = props => (
   <div>
-    <div className="summaryCard" style={{ borderRadius: '5px', border: props.selected ? '8px solid #16abe4' : '8px solid #ffffff', opacity: props.disabled ? '0.25' : (props.selected ? '1.0' : '0.75'), backgroundColor: '#4077a5', marginBottom: '15px' }}>
-      <div style={{ backgroundColor: props.selected ? '#d3f1ff' : '#eeeeee', paddingTop: '15px' }}>
-        <div className="text-center text-primary" style={{ minHeight: '45px' }}>
+    <div className="summaryCard" style={{ borderRadius: '2px', border: props.selected ? '2px solid #16abe4' : '2px solid #ffffff', opacity: props.disabled ? '0.25' : (props.selected ? '1.0' : '0.75'), backgroundColor: '#4077a5', marginBottom: '10px' }}>
+      <div style={{ backgroundColor: props.selected ? '#d3f1ff' : '#eeeeee', paddingTop: '10px' }}>
+        <div className="text-center text-primary" style={{ minHeight: '30px' }}>
           <input type="checkbox" aria-label={[props.category, 'projects'].join(' ')} label={[props.category, 'projects'].join(' ')} value={props.category} checked={props.selected} readOnly />
           <span>{props.category.startsWith('Bond -') ? [props.category.split(' - ')[1].slice(0, -8), 'bond'].join(' ') : props.category}</span>
         </div>

92bf2ec14497d8fc32eef57bb50d2ca75ef6c1bc
Leng <mghuy@unca.edu>
2017-10-27 12:42:26 -0400

    working on checkbox group

--- src/shared/FilterCheckbox.js
+++ src/shared/FilterCheckbox.js
@@ -19,13 +19,23 @@ const backgroundStyle = (checked) => {
   return 'backgroundUncheck';
 };
 
+const handleShifting = (event) => {
+  if (event.charCode === 9) {
+    const active = document.activeElement;
+    event.target.setAttribute('tabIndex', '-1');
+  }
+  if (event.charCode === 9 && event.charCode === 16) {
+
+  }
+}
+
 const FilterCheckbox = props => (
     <div>
       <div className={mainStyle(props.selected, props.disabled)} onClick={props.handleChange}>
         <div className={backgroundStyle(props.selected)} style={{ paddingTop: '10px' }}>
           <div className="text-center text-primary" style={{ minHeight: '30px' }}>
-            <input style={{ marginRight: '7px' }}type="checkbox" aria-label={props.label} label={props.label} value={props.value} checked={props.selected} readOnly />
-            <label>{props.label}</label>
+            <input tabIndex="-1" style={{ marginRight: '7px' }}type="checkbox" aria-label={props.label} label={props.label} value={props.value} checked={props.selected} onKeyPress={handleShifting} readOnly />
+            <label style={{ fontWeight: 'normal' }}>{props.label}</label>
           </div>
         </div>
       </div>
--- src/shared/FilterCheckboxGroup.js
+++ src/shared/FilterCheckboxGroup.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const handleEvent = (event) => {
+  event.preventDefault();
+  if (event.charCode === 32 || event.charCode === 13) {
+    // event.target.setAttribute('tabIndex', '-1');
+
+    const chk = event.target.children;
+    console.log(event.target);
+    console.log(chk[0]);
+    chk[0].focus();
+    const i = 0;
+    for (i; i < chk.length; i + 1) {
+      chk[i].setAttribute('tabIndex', '0');
+    }
+  }
+}
 
 const FilterCheckboxGroup = props => (
-  <div className="row" style={{ backgroundColor: '#ffffff' }} tabIndex="0">
+  <div className="row" style={{ backgroundColor: '#ffffff' }} tabIndex="0" onKeyPress={handleEvent}>
     { props.children }
   </div>
 );

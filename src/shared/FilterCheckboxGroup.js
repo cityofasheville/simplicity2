7a3ddf283e0674f5bc8a11542a6a01e42ca4fb54
Leng Ghuy <lengghuy@asheville.local>
2017-10-27 13:07:01 -0400

    implement checkbox group accessibility

--- src/shared/FilterCheckboxGroup.js
+++ src/shared/FilterCheckboxGroup.js
@@ -6,16 +6,13 @@ const handleEvent = (event) => {
   if (event.charCode === 32 || event.charCode === 13) {
     // event.target.setAttribute('tabIndex', '-1');
 
-    const chk = event.target.children;
-    console.log(event.target);
-    console.log(chk[0]);
+    const chk = event.target.querySelectorAll('input');
     chk[0].focus();
-    const i = 0;
-    for (i; i < chk.length; i + 1) {
+    for (let i = 0; i < chk.length; i += 1) {
       chk[i].setAttribute('tabIndex', '0');
     }
   }
-}
+};
 
 const FilterCheckboxGroup = props => (
   <div className="row" style={{ backgroundColor: '#ffffff' }} tabIndex="0" onKeyPress={handleEvent}>

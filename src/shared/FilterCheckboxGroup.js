9e015df5043314fa3349a954f7f2d0e59c1c06ca
Leng <mghuy@unca.edu>
2017-11-03 15:09:04 -0400

    implement accessible filtercheckbox group

--- src/shared/FilterCheckboxGroup.js
+++ src/shared/FilterCheckboxGroup.js
@@ -1,35 +1,76 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const handleEvent = (event) => {
-  if(event.keyCode == 9) {}
-  else {
-    event.preventDefault();
-    event.target.removeAttribute('tabIndex');
-    if (event.keyCode === 39 || event.keyCode === 40 || event.keyCode === 13) {
-      const chk = event.target.querySelectorAll('input');
-      chk[0].focus();
-      for (let i = 0; i < chk.length; i += 1) {
-        console.log(chk[i]);
-        chk[i].setAttribute('tabIndex', '0');
+class FilterCheckboxGroup extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      focus: 0,
+      firstTime: true,
+      chkArr: null,
+    };
+    this.handleEvent = this.handleEvent.bind(this);
+    //console.log(this.state.chkArr);
+  }
+
+  componentDidMount() {
+    this.setState ({
+      chkArr: document.getElementById(this.props.id).querySelectorAll('input'),
+    });
+  }
+
+  handleEvent (event) {
+    //console.log(this.state.chkArr);
+    if (event.keyCode == 9) {
+    }
+    else if (event.keyCode === 37|| event.keyCode === 38 || event.keyCode === 39 || event.keyCode === 40 || event.keyCode === 13) {
+      event.preventDefault();
+
+      if (document.activeElement === document.getElementById(this.props.id)) {
+        console.log('first time');
+        this.state.chkArr[this.state.focus].focus();
+        this.setState({firstTime: false});
+      }
+      else if (event.keyCode === 37 || event.keyCode === 38) {
+        console.log('go left');
+        this.setState({focus: this.state.focus -= 1,});
+        if (this.state.focus < 0) {
+          document.getElementById(this.props.id).focus();
+          this.setState({focus: 0});
+        }
+        else this.state.chkArr[this.state.focus].focus();
+      }
+      else if (event.keyCode === 39 || event.keyCode === 40) {
+        console.log('go right');
+        this.setState({focus: this.state.focus += 1,});
+        if (this.state.focus === this.state.chkArr.length) {
+          document.getElementById(this.props.id).focus();
+          this.setState({focus: 0});
+        }
+        else this.state.chkArr[this.state.focus].focus();
       }
     }
   }
-};
 
-const FilterCheckboxGroup = props => (
-  <div className="row" style={{ backgroundColor: '#ffffff' }} tabIndex="0" onMouseUp={e => e.target.blur()} onKeyDown={handleEvent}>
-    { props.children }
-  </div>
-);
+  render () {
+    return (
+      <div className="row" id={this.props.id} style={{backgroundColor: '#ffffff'}} tabIndex="0" onMouseUp={e => e.target.blur()} onKeyDown={this.handleEvent}>
+        { this.props.children }
+      </div>
+    );
+  }
+}
 
 FilterCheckboxGroup.propTypes = {
   children: PropTypes.node,
-
+  id: PropTypes.string,
+  focus: PropTypes.number,
 };
 
 FilterCheckboxGroup.defaultProps = {
   children: null,
+  id: 'checkboxGroup',
+  focus: 0,
 };
 
 export default FilterCheckboxGroup;

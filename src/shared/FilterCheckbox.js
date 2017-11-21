1812a375429a3332ba0f821a0091bf780f415665
Leng <mghuy@unca.edu>
2017-11-21 10:52:37 -0500

    Fixed blue focus border

--- src/shared/FilterCheckbox.js
+++ src/shared/FilterCheckbox.js
@@ -13,6 +13,9 @@ class FilterCheckbox extends React.Component {
   }
 
   handleClick(event) {
+    //event.preventDefault();
+    if(this.props.handleChange != null)
+      this.props.handleChange();
     this.setState({
       checked: !this.state.checked,
     });
@@ -38,13 +41,13 @@ class FilterCheckbox extends React.Component {
              onMouseUp={e => e.target.blur()}
              tabIndex="-1">
           <div className={mainStyle} onMouseUp={e => e.target.blur()} tabIndex="-1"
-               onClick={this.props.handleChange}>
-            <div className={backgroundStyle} style={{paddingTop: '10px'}} onClick={this.handleClick}>
-              <div className="text-center text-primary" style={{minHeight: '30px'}}>
+               onClick={this.handleClick}>
+            <div className={backgroundStyle} style={{paddingTop: '10px'}} onMouseUp={e => e.target.blur()} tabIndex="-1">
+              <div className="text-center text-primary" style={{minHeight: '30px'}} onMouseUp={e => e.target.blur()} tabIndex="-1">
                 <input tabIndex="-1" style={{marginRight: '7px'}} type="checkbox" aria-label={this.props.label}
                        label={this.props.label} value={this.props.value} checked={this.state.checked} onKeyDown={this.handleKey} onMouseUp={e => e.target.blur()}
                        readOnly/>
-                <label style={{fontWeight: 'normal', cursor: 'pointer'}}>{this.props.label}</label>
+                <label style={{fontWeight: 'normal', cursor: 'pointer'}} onMouseUp={e => e.target.blur()} tabIndex="-1">{this.props.label}</label>
               </div>
             </div>
           </div>

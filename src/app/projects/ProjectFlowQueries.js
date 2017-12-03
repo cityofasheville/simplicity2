a302abb84c533753dafabd1b1ae4054aa2819c9e
Eric Jackson <ejackson@ashevillenc.gov>
2017-12-03 13:08:22 -0500

    Stopped filtering out incidents

--- src/app/projects/ProjectFlowQueries.js
+++ src/app/projects/ProjectFlowQueries.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export const query = gql`
   query projects {
-    projects(after: "2017-01-01", reqtype: "Service Request", 
+    projects(after: "2017-01-01",  
     status: ["Open", "In Progress", "Pending - IT Resolution", "Pending - Vendor", "Waiting On Requestor"]) {
       ID
       Summary

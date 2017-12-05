471e7466dc82b8375c6d5c30d244fc3cc91cb2de
fruiz7 <frances.ruiz@gmail.com>
2017-12-05 11:55:53 -0500

    hide notes for now, add requestor and adjust after parameter

--- src/app/projects/ProjectFlowQueries.js
+++ src/app/projects/ProjectFlowQueries.js
@@ -2,12 +2,13 @@ import gql from 'graphql-tag';
 
 export const query = gql`
   query projects {
-    projects(after: "2017-01-01",  
+    projects(after: "2015-01-01",  
     status: ["Open", "In Progress", "Pending - IT Resolution", "Pending - Vendor", "Waiting On Requestor"]) {
       ID
       Summary
       AssignedTechnician
       RequestedDate
+      Requestor
       CurrentStatus
       Priority
       Notes

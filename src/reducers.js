69ab1f56f352cf732e8b17517b5d8ad091a20bd6
fruiz7 <frances.ruiz@gmail.com>
2018-01-25 16:20:22 -0500

    upgrade apollo to 2

--- src/reducers.js
+++ src/reducers.js
@@ -5,9 +5,8 @@ import auth from './utilities/auth/authReducers';
 import modal from './app/modal/modalReducers';
 import search from './app/search/searchReducers';
 import budget from './app/budget/budgetReducer';
-import { apollo } from './utilities/gqlClient';
 
-const reducers = combineReducers({ auth, modal, search, budget, apollo });
+const reducers = combineReducers({ auth, modal, search, budget });
 
 export default reducers;
 

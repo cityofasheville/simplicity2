3d51c1e656dbf576b4b4052941a5b22a57b24c13
fruiz7 <frances.ruiz@gmail.com>
2018-02-22 15:18:00 -0500

    Have logo go back home not to city website

--- src/app/CityInfoBar.js
+++ src/app/CityInfoBar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { IndexLink } from 'react-router';
 
 const CityInfoBar = () => (
   <div>
     <div className="navbar navbar-default text-primary navbar-fixed-top" style={{ fontSize: '30px', backgroundColor: '#f6fcff' }}>
       <div className="pull-left" style={{ marginRight: '5px', marginTop: '5px', marginBottom: '5px', marginLeft: '15px' }}>
-        <a href="http://www.ashevillenc.gov" target="_blank"><img src={require('./citylogo-flatblue.png')} width="80px" height="80px" alt="City of Asheville logo"></img></a>
+        <IndexLink to="/"><img src={require('./citylogo-flatblue.png')} width="80px" height="80px" alt="City of Asheville logo"></img></IndexLink>
       </div>
       <div className="navbar-header">
         <div className="pull-left">
           <div style={{ marginTop: '10px', marginBottom: '-22px' }}>Dashboards</div>
-          <a href="http://www.ashevillenc.gov" target="_blank" style={{ fontSize: '12px', fontStyle: 'italic' }}>City of Asheville, NC</a>
+          <IndexLink to="/" style={{ fontSize: '12px', fontStyle: 'italic' }}>City of Asheville, NC</IndexLink>
         </div>
       </div>
     </div>

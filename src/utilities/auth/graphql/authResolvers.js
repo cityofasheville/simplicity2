a4c087ddb79c50581a37716a897f8e26d8fb069a
fruiz7 <frances.ruiz@gmail.com>
2018-03-26 10:01:24 -0400

    eliminate writeToStore warnings with IntrospectionFragmentMatcher (#323)

--- src/utilities/auth/graphql/authResolvers.js
+++ src/utilities/auth/graphql/authResolvers.js
@@ -5,6 +5,7 @@ export const authResolvers = {
     updateUser: (_, { loggedIn, privilege, name, email, provider }, { cache }) => {
       const data = {
         user: {
+          __typename: 'user',
           loggedIn,
           privilege,
           name,
@@ -13,22 +14,27 @@ export const authResolvers = {
         },
       };
       cache.writeQuery({ query: getUser, data });
+      return data.user;
     },
     updateAuthModal: (_, { open }, { cache }) => {
       const data = {
         modal: {
+          __typename: 'authModal',
           open,
         },
       };
       cache.writeQuery({ query: getModalOpen, data });
+      return data.modal;
     },
     updateAuthDropdown: (_, { open }, { cache }) => {
       const data = {
         dropdown: {
+          __typename: 'authDropdown',
           open,
         },
       };
       cache.writeQuery({ query: getDropdownOpen, data });
+      return data.dropdown;
     },
   },
 };

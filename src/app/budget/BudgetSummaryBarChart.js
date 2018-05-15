f104a73ecd490353426d6733bfac097cd001437a
fruiz7 <frances.ruiz@gmail.com>
2018-05-15 13:12:06 -0400

    Fix some budget page errors

--- src/app/budget/BudgetSummaryBarChart.js
+++ src/app/budget/BudgetSummaryBarChart.js
@@ -65,36 +65,24 @@ class BudgetSummaryBarChart extends React.Component {
       <div>
         <div className="row">
           <div className="col-sm-12">
-            <BarChart
-              annotations={createAnnotations(this.props.data.budgetParameters)}
-              data={this.state.summaryData.dataValues}
-              colorScheme={this.props.colorScheme}
-              mainAxisDataKey="display_year"
-              dataKeys={this.state.summaryData.dataKeys}
-              yAxisTickFormatter={getDollars}
-              tooltipYValFormatter={getDollarsLong}
-              domain={[0, 190000000]}
-              legendLabelFormatter={function(label) {return label.replace(' Department', '')}}
-              altText={['Spending by', this.props.categoryType, 'bar chart'].join(' ')}
-              chartTitle={getTitle(this.props.categoryType)}
-              dataFormatter={formatBudgetDataForStackedBar}
-            />
+            <div className="visualization no-padding">
+              <BarChart
+                annotations={createAnnotations(this.props.data.budgetParameters)}
+                data={this.state.summaryData.dataValues}
+                colorScheme={this.props.colorScheme}
+                mainAxisDataKey="display_year"
+                dataKeys={this.state.summaryData.dataKeys}
+                yAxisTickFormatter={getDollars}
+                tooltipYValFormatter={getDollarsLong}
+                domain={[0, 190000000]}
+                legendLabelFormatter={function(label) {return label.replace(' Department', '')}}
+                altText={['Spending by', this.props.categoryType, 'bar chart'].join(' ')}
+                chartTitle={getTitle(this.props.categoryType)}
+                dataFormatter={formatBudgetDataForStackedBar}
+              />
+            </div>
           </div>
         </div>
-      <div className="visualization no-padding">
-        <BarChart
-          annotations={createAnnotations(this.props.data.budgetParameters)}
-          data={this.state.summaryData.dataValues}
-          colorScheme={this.props.colorScheme}
-          mainAxisDataKey="display_year"
-          dataKeys={this.state.summaryData.dataKeys}
-          yAxisTickFormatter={getDollars}
-          tooltipYValFormatter={getDollarsLong}
-          domain={[0, 190000000]}
-          legendLabelFormatter={function(label) {return label.replace(' Department', '')}}
-          altText={['Spending by', this.props.categoryType, 'bar chart'].join(' ')}
-          chartTitle={getTitle(this.props.categoryType)}
-        />
       </div>
     );
   }

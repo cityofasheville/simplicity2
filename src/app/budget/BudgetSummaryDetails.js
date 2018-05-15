6997ca6314a07f30532e290b253726dc4ea86c36
fruiz7 <frances.ruiz@gmail.com>
2018-05-15 14:33:35 -0400

    AVL-RN update budget link for proposed 18-19 (#361)

--- src/app/budget/BudgetSummaryDetails.js
+++ src/app/budget/BudgetSummaryDetails.js
@@ -11,7 +11,7 @@ import { IM_COIN_DOLLAR } from '../../shared/iconConstants';
 const BudgetSummaryDetails = (props) => {
   return (
     <div>
-      <PageHeader h1={props.data.budgetParameters.in_budget_season ? `Proposed budget ${parseInt(props.data.budgetParameters.end_year, 10) - 1}-${props.data.budgetParameters.end_year}` : `Budget ${parseInt(props.data.budgetParameters.end_year, 10) - 1}-${props.data.budgetParameters.end_year}`} externalLinkText="Full budget document" externalLink="http://www.ashevillenc.gov/civicax/filebank/blobdload.aspx?blobid=28117" dataLinkPath="/budget/data" icon={<Icon path={IM_COIN_DOLLAR} size={60} />}>
+      <PageHeader h1={props.data.budgetParameters.in_budget_season ? `Proposed budget ${parseInt(props.data.budgetParameters.end_year, 10) - 1}-${props.data.budgetParameters.end_year}` : `Budget ${parseInt(props.data.budgetParameters.end_year, 10) - 1}-${props.data.budgetParameters.end_year}`} externalLinkText="Full budget document" externalLink="http://www.ashevillenc.gov/civicax/filebank/blobdload.aspx?blobid=30387" dataLinkPath="/budget/data" icon={<Icon path={IM_COIN_DOLLAR} size={60} />}>
         <ButtonGroup alignment="">
           <LinkButton pathname="/budget" query={{ entity: props.location.query.entity, id: props.location.query.id, label: props.location.query.label, hideNavbar: props.location.query.hideNavbar }} positionInGroup="left">Summary</LinkButton>
           <LinkButton pathname="/budget/details" query={{ entity: props.location.query.entity, id: props.location.query.id, label: props.location.query.label, mode: props.location.query.mode || 'expenditures', hideNavbar: props.location.query.hideNavbar }} positionInGroup="right" active>Details</LinkButton>

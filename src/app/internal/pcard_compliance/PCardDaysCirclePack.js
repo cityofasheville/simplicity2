41cf6004e5de72a9f0b6a3ef3702cb958c52703a
fruiz7 <frances.ruiz@gmail.com>
2018-06-04 14:21:10 -0400

    AVL-RN: Pcard dashboard demo version (#371)
    * quick table and initial start on sunburst
    * More progress
    * Fix preliminary layouts, add basic filters for demo
    * Set demo date range
    * more progress
    * More progress on dashboard
    * receipts page for pcard compliance dashboard
    * switch to Query components and general code cleanup
    * Add temporary email check so only users logged in with specific emails can review, for finance to look at. Fix some small bugs with colors and div widths
    * Remove objectAssign when can use Object.assign

---
+++ src/app/internal/pcard_compliance/PCardDaysCirclePack.js
@@ -0,0 +1,166 @@
+import React from 'react';
+import { NetworkFrame, OrdinalFrame } from 'semiotic';
+
+const colorHash = {
+  '0-30 days': '#4575b4',
+  '31-60 days': '#fdae61',
+  '61-90 days': '#f46d43',
+  '>90 days': '#d73027',
+};
+
+const getColor = (d) => {
+  if (d.over_90 > 0) {
+    return '#d73027';
+  }
+  if (d.under_90 > 0) {
+    return '#f46d43';
+  }
+  if (d.under60 > 0) {
+    return '#fdae61';
+  }
+  return '#4575b4';
+};
+  // '#d73027', '#fdae61', '#abd939', ];
+
+const sumChildren = (d) => {
+  let under30 = 0;
+  let under60 = 0;
+  let under90 = 0;
+  let over90 = 0;
+
+  d.children.forEach((child) => {
+    under30 += child.under30;
+    under60 += child.under60;
+    under90 += child.under90;
+    over90 += child.over90;
+  });
+
+  return (
+    <div
+      className="pull-right"
+      style={{ fontWeight: 200, marginRight: '10px' }}
+    >
+      <div style={{ color: colorHash['0-30 days'] }}>0-30 days: {under30}</div>
+      <div style={{ color: colorHash['31-60 days'] }}>31-60 days: {under60}</div>
+      <div style={{ color: colorHash['61-90 days'] }}>61-90 days: {under90}</div>
+      <div style={{ color: colorHash['>90 days'] }}>&gt;90 days: {over90}</div>
+    </div>
+  );
+};
+
+const sumPieData = (d) => {
+  let under30 = 0;
+  let under60 = 0;
+  let under90 = 0;
+  let over90 = 0;
+
+  d.children.forEach((child) => {
+    under30 += child.under30;
+    under60 += child.under60;
+    under90 += child.under90;
+    over90 += child.over90;
+  });
+
+  return [
+    { days: '0-30 days', count: under30 },
+    { days: '31-60 days', count: under60 },
+    { days: '61-90 days', count: under90 },
+    { days: '>90 days', count: over90 },
+  ];
+};
+
+const PCardDaysCirclePack = props => (
+  <div
+    style={{ flexBasis: '35%' }}
+  >
+    <div style={{ marginLeft: '-50px' }}>
+      <NetworkFrame
+        size={[400, 400]}
+        edges={props.data}
+        nodeStyle={d => ({
+          fill: getColor(d),
+          stroke: 'black',
+          strokeOpacity: 0.25,
+          fillOpacity: 0.25,
+        })}
+        edgeStyle={d => ({
+          fill: getColor(d),
+          stroke: getColor(d),
+          opacity: 0.5,
+        })}
+        networkType={{
+          type: 'circlepack',
+          projection: 'vertical',
+          padding: 0,
+          hierarchySum: d => d.totalCount,
+        }}
+        nodeIDAccessor="name"
+        hoverAnnotation
+        tooltipContent={d => (
+          <div
+            style={{
+              position: 'absolute',
+              left: window.innerWidth <= 989 ? -d.x : 330 - d.x,
+              top: window.innerWidth <= 989 ? -d.y + 350 : -d.y - 50,
+              height: '250px',
+              background: '#f1f1f1',
+              paddingLeft: '5px',
+            }}
+          >
+            <p>
+              {
+                d.data.breadcrumbPath === undefined ?
+                  d.dept
+                  :
+                  <span>{[...new Set(d.data.breadcrumbPath.split('>').map(element => element.trim()))].slice(1).join(' > ')}</span>
+              }
+            </p>
+            <div
+              style={{ width: '435px', height: '250px' }}
+            >
+              <div className="pull-left">
+                <OrdinalFrame
+                  size={[175, 175]}
+                  data={
+                    d.parent ?
+                      [
+                        { days: '0-30 days', count: d.data.under30 },
+                        { days: '31-60 days', count: d.data.under60 },
+                        { days: '61-90 days', count: d.data.under90 },
+                        { days: '>90 days', count: d.data.over90 },
+                      ]
+                      :
+                      sumPieData(d)
+                  }
+                  oAccessor="days"
+                  dynamicColumnWidth="count"
+                  style={pd => ({ fill: colorHash[pd.days], stroke: 'white' })}
+                  type="bar"
+                  projection="radial"
+                  oLabel={false}
+                />
+              </div>
+              {
+                d.parent ?
+                  <div
+                    className="pull-right"
+                    style={{ fontWeight: 200, marginRight: '10px' }}
+                  >
+                    <div style={{ color: colorHash['0-30 days'] }}>0-30 days: {d.data.under30}</div>
+                    <div style={{ color: colorHash['31-60 days'] }}>31-60 days: {d.data.under60}</div>
+                    <div style={{ color: colorHash['61-90 days'] }}>61-90 days: {d.data.under90}</div>
+                    <div style={{ color: colorHash['>90 days'] }}>&gt;90 days: {d.data.over90}</div>
+                  </div>
+                  :
+                  sumChildren(d)
+              }
+            </div>
+          </div>
+        )}
+        margin={50}
+      />
+    </div>
+  </div>
+);
+
+export default PCardDaysCirclePack;
---
+++ src/app/internal/pcard_compliance/pcard_compliance_utilities.js
@@ -0,0 +1,242 @@
+import tree from 'data-tree';
+
+const levels = [
+  'dept_id',
+  'div_id',
+  'cardholder',
+  // 'statement_id',
+];
+
+// level names
+const levelNames = [
+  'department',
+  'division',
+  'cardholder',
+  // 'statement_id',
+];
+
+// flatten the tree and export for use by the budget Treemap
+const exportForDetails = (aTree) => {
+  const flattened = aTree.export(data => (Object.assign({}, data)));
+  return flattened;
+};
+
+// helper function to build the tree for the Treemap
+const searchChildrenForKey = (aKey, aTreeNode) => {
+  const children = aTreeNode.childNodes();
+  for (let i = 0; i < children.length; i += 1) {
+    if (children[i].data().key === aKey) {
+      return children[i];
+    }
+  }
+  return null;
+};
+
+// helper function to insert copies of leaf nodes below themselves (in the flattened tree)
+const insertLeafCopies = (flattenedTree) => {
+  if (flattenedTree.children.length === 0) {
+    const splitBreadcrumbPath = flattenedTree.breadcrumbPath.split('>');
+    const splitPath = flattenedTree.path.split('_');
+    flattenedTree.children = [Object.assign({}, flattenedTree, { breadcrumbPath: [flattenedTree.breadcrumbPath, splitBreadcrumbPath[splitBreadcrumbPath.length - 1]].join('>') }, { path: [flattenedTree.path, splitPath[splitPath.length - 1]].join('_') })];
+  } else {
+    for (let i = 0; i < flattenedTree.children.length; i += 1) {
+      insertLeafCopies(flattenedTree.children[i]);
+    }
+  }
+};
+
+export const buildReceiptsTree = (data) => {
+  const theTree = tree.create();
+  theTree.insert({ key: 'root' });
+  for (let i = 0; i < data.length; i += 1) {
+    let curNode = theTree.rootNode();
+    let curParent = theTree.rootNode();
+    for (let j = 0; j < levels.length; j += 1) {
+      curNode = searchChildrenForKey(data[i][levels[j]], curParent);
+      let curPath = 'root';
+      let breadCrumbPath = 'root';
+      for (let k = 0; k <= j; k += 1) {
+        curPath = [curPath, encodeURIComponent(data[i][levels[k]])].join('_');
+        breadCrumbPath = [breadCrumbPath, data[i][levelNames[k]]].join('>');
+      }
+      if (curNode === null) {
+        curNode = theTree.insertToNode(curParent, {
+          key: data[i][levels[j]],
+          path: curPath,
+          breadcrumbPath: breadCrumbPath,
+          [levels[j]]: data[i][levels[j]],
+          name: data[i][levelNames[j]],
+          has_itemized_receipt: 0,
+          missing_itemized_receipt: 0,
+          total_receipts: 0,
+        });
+      }
+      curParent = curNode;
+      curNode.data(Object.assign(
+        {},
+        curNode.data(),
+        {
+          has_itemized_receipt: data[i].receipt === 'Y' ? curNode.data().has_itemized_receipt + 1 : curNode.data().has_itemized_receipt,
+        },
+        {
+          missing_itemized_receipt: data[i].receipt !== 'Y' ? curNode.data().missing_itemized_receipt + 1 : curNode.data().missing_itemized_receipt,
+        },
+        {
+          total_receipts: curNode.data().total_receipts + 1,
+        }
+      ));
+    }
+  }
+  const exTree = exportForDetails(theTree);
+  insertLeafCopies(exTree);
+  exTree.children.sort((a, b) => {
+    let compareA = null;
+    let compareB = null;
+    if (a === null || a.name === null) {
+      compareA = '';
+    }
+    if (b === null || b.name === null) {
+      compareB = '';
+    }
+    if (compareA !== '') {
+      compareA = a.name.toLowerCase();
+    }
+    if (compareB !== '') {
+      compareB = b.name.toLowerCase();
+    }
+    if (compareA < compareB) {
+      return -1;
+    }
+    if (compareA > compareB) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return exTree;
+};
+
+// builds the tree
+export const buildTree = (data) => {
+  const theTree = tree.create();
+  theTree.insert({ key: 'root' });
+  for (let i = 0; i < data.length; i += 1) {
+    let curNode = theTree.rootNode();
+    let curParent = theTree.rootNode();
+    for (let j = 0; j < levels.length; j += 1) {
+      curNode = searchChildrenForKey(data[i][levels[j]], curParent);
+      let curPath = 'root';
+      let breadCrumbPath = 'root';
+      for (let k = 0; k <= j; k += 1) {
+        curPath = [curPath, encodeURIComponent(data[i][levels[k]])].join('_');
+        breadCrumbPath = [breadCrumbPath, data[i][levelNames[k]]].join('>');
+      }
+      if (curNode === null) {
+        curNode = theTree.insertToNode(curParent, {
+          key: data[i][levels[j]],
+          path: curPath,
+          breadcrumbPath: breadCrumbPath,
+          [levels[j]]: data[i][levels[j]],
+          name: data[i][levelNames[j]],
+          under30: 0,
+          under60: 0,
+          under90: 0,
+          over90: 0,
+          totalCount: 0,
+          //statements: [],
+        });
+      }
+      curParent = curNode;
+      let daysOpen = 0;
+      if (data[i].reconciled_date !== null) {
+        daysOpen = data[i].days_invoiced_to_reconciled;
+      } else {
+        daysOpen = data[i].days_since_reconciled;
+      }
+      curNode.data(Object.assign(
+        {},
+        curNode.data(),
+        {
+          under30: daysOpen <= 30 ? curNode.data().under30 + 1 : curNode.data().under30,
+        },
+        {
+          under60: daysOpen > 30 && daysOpen <= 60 ?
+            curNode.data().under60 + 1 : curNode.data().under60,
+        },
+        {
+          under90: daysOpen > 60 && daysOpen <= 90 ?
+            curNode.data().under90 + 1 : curNode.data().under90,
+        },
+        {
+          over90: daysOpen > 90 ? curNode.data().over90 + 1 : curNode.data().over90,
+        },
+        {
+          totalCount: curNode.data().totalCount + 1,
+        },
+        // {
+        //   statements: curNode.data().statements.concat([{
+        //     statement_code: data[i].statement_code,
+        //     statement_id: data[i].statement_id,
+        //     statement_status: data[i].statement_status,
+        //     fiscal_year: data[i].fiscal_year,
+        //     fiscal_period: data[i].fiscal_period,
+        //     invoiced_date: data[i].invoiced_date,
+        //     reconciled_date: data[i].reconciled_date,
+        //     days_invoiced_to_reconciled: data[i].days_invoiced_to_reconciled,
+        //     approved_date: data[i].approved_date,
+        //     days_reconciled_to_approved: data[i].days_reconciled_to_approved,
+        //     days_since_invoiced: data[i].days_since_invoiced,
+        //     days_since_reconciled: data[i].days_since_reconciled,
+        //   }]),
+        // }
+      ));
+    }
+  }
+  const exTree = exportForDetails(theTree);
+  insertLeafCopies(exTree);
+  exTree.children.sort((a, b) => {
+    let compareA = null;
+    let compareB = null;
+    if (a === null || a.name === null) {
+      compareA = '';
+    }
+    if (b === null || b.name === null) {
+      compareB = '';
+    }
+    if (compareA !== '') {
+      compareA = a.name.toLowerCase();
+    }
+    if (compareB !== '') {
+      compareB = b.name.toLowerCase();
+    }
+    if (compareA < compareB) {
+      return -1;
+    }
+    if (compareA > compareB) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return exTree;
+};
+
+export const findTop = (data, path) => {
+  const nodes = path.split('_');
+  if (nodes.length === 1) {
+    return data.children;
+  }
+  let curNode = data;
+  let prevNode = null;
+  for (let i = 0; i < nodes.length; i += 1) {
+    for (let j = 0; j < curNode.children.length; j += 1) {
+      if (curNode.children[j].key === nodes[i]) {
+        prevNode = curNode;
+        curNode = curNode.children[j];
+        break;
+      }
+    }
+  }
+  return ((curNode.children === undefined || curNode.children.length === 0) ?
+    prevNode.children : curNode.children);
+};

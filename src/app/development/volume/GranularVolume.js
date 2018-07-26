31ad3bc6cbf9af7b4c495cc13c998974abf323d7
mmazanec22 <melanie.mazanec@gmail.com>
2018-07-26 10:02:57 -0400

    Create granular volume component

---
+++ src/app/development/volume/GranularVolume.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class GranularVolume extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hover: null,
+    };
+  }
+
+  render() {
+    return <div>Foo!</div>;
+  }
+}
+
+export default GranularVolume;

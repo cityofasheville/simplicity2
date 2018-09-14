8c4ef783f9a7bb20773b52e307ac9f334a3c167a
mmazanec22 <melanie.mazanec@gmail.com>
2018-09-14 13:30:34 -0400

    Open child menus... but never close them

--- src/app/development/volume/HierarchicalDropdown.js
+++ src/app/development/volume/HierarchicalDropdown.js
@@ -3,52 +3,88 @@ import PropTypes from 'prop-types';
 
 
 
-const addChildren = (node, onNodeClick) => {
-  const concatenatedHeritage = node.heritage.concat([node.key]).join();
-  return (<li
-    style={{ marginLeft: node.depth * 5 }}
-    className={node.values ? 'dropdown-submenu dropdown-menu-right' : ''}
-    key={concatenatedHeritage}
-  >
-    <a
-      value={concatenatedHeritage}
-      href=""
-      onClick={(e) => {
-        e.preventDefault();
-        onNodeClick(node);
-      }}
-    >
-      {node.key}
-    </a>
-      {node.values &&(
-        <ul
-          className="dropdown-menu dropdown-menu-right"
-          role="menu"
+class ChildMenus extends Component {
+  state = {
+    open: false,
+  }
+  render() {
+    const concatenatedHeritage = this.props.node.heritage
+      .concat([this.props.node.key])
+      .join();
+    let className = '';
+    if (this.props.node.values) {
+      className = `${className} dropdown-submenu dropdown-menu-right`
+    }
+    if (this.state.open) {
+      className = `${className} open`
+    }
+    return (
+      <li
+        style={{ marginLeft: this.props.node.depth * 5 }}
+        className={className}
+        key={concatenatedHeritage}
+        onMouseOver={() => this.setState({
+          open: true,
+        })}
+      >
+        <a
+          value={concatenatedHeritage}
+          href=""
+          onClick={(e) => {
+            e.preventDefault();
+            this.props.onNodeClick(node);
+          }}
+          style={{
+            color: this.props.node.selected ? '#00a4f6' : 'inherit',
+          }}
         >
-          {node.values.map((child) => {
-            return addChildren(child);
-          })}
-        </ul>
-      )}
-  </li>
-)
+          {this.props.node.key}
+        </a>
+        {this.props.node.values &&(
+          <ul
+            className="dropdown-menu dropdown-menu-right"
+            role="menu"
+          >
+            {this.props.node.values.map((child) => {
+              return (<ChildMenus
+                key={child.key}
+                node={child}
+                onNodeClick={this.props.onNodeClick}
+              />);
+            })}
+          </ul>
+        )}
+      </li>
+    )
+  }
 }
 
 
 class HierarchicalDropdown extends Component {
+  /* TODO
+    make menus expand on hover of parent menu
+    make colors match what's happening in parent component
+      assign them in the depth labels?
+      both gray vs colorful and opacity
+      hover style should also match nodes
+      focus style should match hover style
+  */
   state = {
     open: false,
   }
 
   render() {
-    return (<div className={`btn-group ${this.state.open ? 'open' : ''}`}>
+    return (<div
+      className={`btn-group ${this.state.open ? 'open' : ''}`}
+    >
       <a
-        className="btn dropdown-toggle"
+        className="dropdown-toggle"
         href=""
         onClick={(e) => {
           e.preventDefault();
-          this.setState({ open: true })
+          this.setState({ open: !this.state.open })
         }}
+        onBlur={() => this.setState({ open: false })}
         >
           Permit Types
           <span className="caret"></span>
@@ -58,7 +94,13 @@ class HierarchicalDropdown extends Component {
           role="menu"
           >
             {
-              this.props.hierarchy.values.map(value => addChildren(value, this.props.onNodeClick))
+              this.props.hierarchy.values.map(node => (
+                <ChildMenus
+                  key={node.key}
+                  node={node}
+                  onNodeClick={this.props.onNodeClick}
+                />
+              ))
             }
           </ul>
         </div>)

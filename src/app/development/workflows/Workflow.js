aa90deec97b05ce9d0eaaf7998f05e2c7a0caf15
mmazanec22 <melanie.mazanec@gmail.com>
2018-10-22 11:22:10 -0400

    Make it a class in preparation for click behavior

--- src/app/development/workflows/Workflow.js
+++ src/app/development/workflows/Workflow.js
@@ -6,25 +6,33 @@ import { scaleLinear } from 'd3-scale';
 import { ResponsiveNetworkFrame } from 'semiotic';
 
 
-const Workflow = (props) => {
-  const uniquePermitsNest = nest()
+class Workflow extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      deptDetailShowing: null,
+    }
+  }
+
+  render() {
+    const uniquePermitsNest = nest()
     .key(d => d.permit_number);
 
-  const typeNest = nest()
+    const typeNest = nest()
     .key(d => d.permit_group)
     .key(d => d.permit_type)
     // .key(d => d.permit_subtype)
     // .key(d => d.permit_category)
     .rollup(d => d.length)
 
-  const deptNest = nest()
+    const deptNest = nest()
     .key(d => d.user_department)
 
-  const personNest = nest()
+    const personNest = nest()
     .key(d => d.user_name)
 
-  const nestedData = deptNest
-    .entries(props.data)
+    const nestedData = deptNest
+    .entries(this.props.data)
     .sort((a, b) => b.values.length - a.values.length)
     .map(dept => {
       // dept.children = personNest.entries(dept.values)
@@ -39,75 +47,70 @@ const Workflow = (props) => {
       return dept;
     })
 
-  const nestedWithRoot = {
-    key: 'All Tasks',
-    values: props.data,
-    children: nestedData,
-    // uniquePermits: uniquePermitsNest.entries(props.data),
-    byType: typeNest.entries(props.data),
-  }
+    const nestedWithRoot = {
+      key: 'All Tasks',
+      values: this.props.data,
+      children: nestedData,
+      // uniquePermits: uniquePermitsNest.entries(this.props.data),
+      byType: typeNest.entries(this.props.data),
+    }
 
-  // TODO: figure out how to get correct size of root
-  const rootSize = 200
-  const nodeSizeFunc = scaleLinear()
+    // TODO: figure out how to get correct size of root
+    const rootSize = 200
+    const nodeSizeFunc = scaleLinear()
     .range([2, rootSize])
-    .domain([0, props.data.length]);
+    .domain([0, this.props.data.length]);
 
-
-  console.log(nestedWithRoot)
-
-  // scale size by volume of all permits ?
-
-
-  return (<div className="dashRows">
-    <div>
-      <ResponsiveNetworkFrame
-        size={[900, 500]}
-        responsiveWidth
-        edges={nestedWithRoot}
-        edgeStyle={{ stroke: 'gray' }}
-        nodeIDAccessor="key"
-        hoverAnnotation
-        nodeLabels
-        nodeSizeAccessor={d => {
-          return nodeSizeFunc(d.values.length)
-        }}
-        customNodeIcon={d => {
-          const size = nodeSizeFunc(d.d.values.length)
-          return (
-            <foreignObject
+    return (<div className="dashRows">
+      <div>
+        <ResponsiveNetworkFrame
+          size={[900, 500]}
+          responsiveWidth
+          edges={nestedWithRoot}
+          edgeStyle={{ stroke: 'gray' }}
+          nodeIDAccessor="key"
+          hoverAnnotation
+          nodeLabels
+          nodeSizeAccessor={d => {
+            return nodeSizeFunc(d.values.length)
+          }}
+          customNodeIcon={d => {
+            const size = nodeSizeFunc(d.d.values.length)
+            return (
+              <foreignObject
               key={d.key}
               x={d.d.x - size / 2}
               y={d.d.y - size / 2}
               width={size}
               height={size}
-            >
+              >
               <ResponsiveNetworkFrame
-                key={d.key}
-                size={[size, size]}
-                edges={{ key: 'root', values: d.d.data.byType }}
-                nodeStyle={{ fill: 'white', stroke: 'black' }}
-                nodeIDAccessor="key"
-                hoverAnnotation
-                networkType={{
-                  type: 'circlepack',
-                  hierarchyChildren: datum => datum.values,
-                  hierarchySum: datum => datum.value,
-                }}
+              key={d.key}
+              size={[size, size]}
+              edges={{ key: 'root', values: d.d.data.byType }}
+              nodeStyle={{ fill: 'white', stroke: 'black' }}
+              nodeIDAccessor="key"
+              hoverAnnotation
+              networkType={{
+                type: 'circlepack',
+                hierarchyChildren: datum => datum.values,
+                hierarchySum: datum => datum.value,
+              }}
               />
-            </foreignObject>
-          )
-        }}
-        networkType={{
-          type: "tree",
-          projection: "vertical",
-          nodePadding: 5,
-          hierarchySum: d => d.values.length,
-        }}
-        margin={50}
-      />
-    </div>
-  </div>);
-};
+              </foreignObject>
+            )
+          }}
+          networkType={{
+            type: "tree",
+            projection: "vertical",
+            nodePadding: 5,
+            hierarchySum: d => d.values.length,
+          }}
+          margin={50}
+        />
+      </div>
+    </div>);
+  }
+}
 
 export default Workflow;

f2bc544a41a1a5b793136625ee22598afb3b1a53
mmazanec22 <melanie.mazanec@gmail.com>
2018-12-13 18:53:03 -0500

    Deal with bootstrap Roboto slowness maybe?

--- src/app/internal/bpt_projects/ProjectFlow.js
+++ src/app/internal/bpt_projects/ProjectFlow.js
@@ -17,21 +17,21 @@ import { refreshLocation } from '../../../utilities/generalUtilities';
 const getIcon = (type) => {
   switch (type) {
     case 'Priority 1':
-      return <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><text x="7" y="24" fontFamily="Roboto" fontSize="24" fill="#fff">1</text></svg>;
+      return <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><text x="7" y="24" fontFamily="sans-serif" fontSize="24" fill="#fff">1</text></svg>;
     case 'Priority 2':
-      return <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><text x="7" y="24" fontFamily="Roboto" fontSize="24" fill="#fff">2</text></svg>;
+      return <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><text x="7" y="24" fontFamily="sans-serif" fontSize="24" fill="#fff">2</text></svg>;
     case 'Priority 3':
-      return <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><text x="7" y="24" fontFamily="Roboto" fontSize="24" fill="#fff">3</text></svg>;
+      return <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><text x="7" y="24" fontFamily="sans-serif" fontSize="24" fill="#fff">3</text></svg>;
     case 'Project Small  5-20 Hrs':
     case 'Project Small':
     case 'Project Small 5-20 Hrs':
-      return <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><text x="7" y="24" fontFamily="Roboto" fontSize="24" fill="#fff">S</text></svg>;
+      return <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><text x="7" y="24" fontFamily="sans-serif" fontSize="24" fill="#fff">S</text></svg>;
     case 'Project Large':
     case 'Project Larger 100 Hrs':
-      return <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><text x="7" y="24" fontFamily="Roboto" fontSize="24" fill="#fff">L</text></svg>;
+      return <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><text x="7" y="24" fontFamily="sans-serif" fontSize="24" fill="#fff">L</text></svg>;
     case 'Project Medium':
     case 'Project Medium 20-100 Hrs':
-      return <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><text x="4" y="24" fontFamily="Roboto" fontSize="24" fill="#fff">M</text></svg>;
+      return <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><text x="4" y="24" fontFamily="sans-serif" fontSize="24" fill="#fff">M</text></svg>;
     default:
       return <svg xmlns="http://www.w3.org/2000/svg" height="25px" transform="translate(0,4)" version="1.1" viewBox="0 0 16 16" width="25px"><g fill="none" fillRule="evenodd" id="Icons with numbers" stroke="none" strokeWidth="1"><g fill="#fff" id="Group" transform="translate(-528.000000, -576.000000)"><path d="M536,592 C531.581722,592 528,588.418278 528,584 C528,579.581722 531.581722,576 536,576 C540.418278,576 544,579.581722 544,584 C544,588.418278 540.418278,592 536,592 Z M541,586 C542.10457,586 543,585.10457 543,584 C543,582.89543 542.10457,582 541,582 C539.89543,582 539,582.89543 539,584 C539,585.10457 539.89543,586 541,586 Z M531,586 C532.10457,586 533,585.10457 533,584 C533,582.89543 532.10457,582 531,582 C529.89543,582 529,582.89543 529,584 C529,585.10457 529.89543,586 531,586 Z M536,586 C537.10457,586 538,585.10457 538,584 C538,582.89543 537.10457,582 536,582 C534.89543,582 534,582.89543 534,584 C534,585.10457 534.89543,586 536,586 Z M536,586" id="Oval 12 copy" /></g></g></svg>;
   }
@@ -210,4 +210,3 @@ export default graphql(query, {
     },
   }),
 })(ProjectFlowDashboard);
-

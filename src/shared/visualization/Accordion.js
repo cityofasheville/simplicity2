08eb075342f7cfae6bc523549a0ef415e77f91d8
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-02-12 16:52:53 -0500

    Accordion no longer re renders

---
+++ src/shared/visualization/Accordion.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/*
+TODO:
+allow for more than one - make id unique
+*/
+
+class AccordionPanel extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      open: false,
+    }
+  }
+
+  render() {
+    const panelHeadingId = `accordion-heading-${this.props.index}`;
+    const collapsibleId = `accordion-button-${this.props.index}`;
+
+    return (<div className="accordion-item-set" key={`accordion-item-${this.props.index}`}>
+      <div className="panel panel-default">
+        <div className="panel-heading" role="tab" id={panelHeadingId}>
+          <h4 className="panel-title">
+            <a
+              role="button"
+              data-toggle="collapse"
+              data-parent="#accordion"
+              href={`#${collapsibleId}`}
+              aria-expanded={this.state.open}
+              aria-controls={collapsibleId}
+              onClick={e => {e.preventDefault(); this.setState({ open: !this.state.open })}}
+            >
+              {this.props.header}
+            </a>
+          </h4>
+        </div>
+        <div
+          id={collapsibleId}
+          className={`panel-collapse collapse${this.state.open ? ' in' : ''}`}
+          role="tabpanel"
+          aria-labelledby={panelHeadingId}
+        >
+          <div className="panel-body">
+            {this.props.body}
+          </div>
+        </div>
+      </div>
+    </div>)
+  }
+}
+
+const Accordion = (props) => (
+  <div
+    className="panel-group"
+    id="accordion"
+    role="tablist"
+    aria-multiselectable="true"
+  >
+  {/* https://getbootstrap.com/docs/3.4/javascript/#collapse */}
+  {/* TODO: WHY DOES COLLAPSE TRIGGER RE-RENDER? */}
+  {props.data.map((d, i) => (<AccordionPanel
+      index={i}
+      header={d.header}
+      body={d.body}
+    />))}
+  </div>
+)
+
+export default Accordion;

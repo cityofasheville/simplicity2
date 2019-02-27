8be48b50d4fd67a29a30b653776aa174e3374824
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-02-27 17:15:21 -0500

    Comment out unused code

--- src/app/development/permits/Permit.js
+++ src/app/development/permits/Permit.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
 import moment from 'moment';
 import { Query } from 'react-apollo';

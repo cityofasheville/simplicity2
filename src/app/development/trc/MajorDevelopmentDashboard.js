d5c2f2ba0ec373793efdc7de6c96d52cc91f0164
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-04-03 16:26:39 -0400

    Change text to be clearer and more concise

--- src/app/development/trc/MajorDevelopmentDashboard.js
+++ src/app/development/trc/MajorDevelopmentDashboard.js
@@ -20,13 +20,25 @@ class MajorDevelopmentDashboard extends React.Component {
         linkId: 'about',
         linkName: 'About',
         selected: true,
-        header: 'About',
+        header: 'Get notifications, attend meetings, and have your voice heard',
         body: (
           <div>
-            <p>When someone wants to build or modify a building on private property in the City of Asheville, they must comply with federal, state, county, and city standards. Which standards apply depends on how large the building is or how much the building will be modified.</p>
-            <p>After the developer submits an application, it goes through a decision-making process that includes city staff, city council, developers, and residents.  Who is involved at what step depends on the type of project.</p>
+            <p>There is a lot of private land development happening in Asheville.  This tool can help you:</p>
+            <ul>
+              <li>
+                Understand the process
+              </li>
+              <li>
+                Be notified when someone applies for a permit to build something near you
+              </li>
+              <li>
+                Find public meetings so that your voice can be heard
+              </li>
+            </ul>
             <p>The Unified Development Ordinance defines six types of large scale development in Asheville.</p>
             <PermitTypeCards/>
+            <br/>
+            <p>After the developer submits an application, it goes through a decision-making process that includes city staff, city council, developers, and residents.  Who is involved at what step depends on the type of project.</p>
             <AnnotatedDagre />
           </div>
         ),

3b5df9d04c4935ae39e2891b61bfa52cb0b57ae0
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-05-20 10:12:36 -0400

    Refactor permit field formatters

--- src/app/development/permits/Permit.js
+++ src/app/development/permits/Permit.js
@@ -1,15 +1,13 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
-import moment from 'moment';
 import { Query } from 'react-apollo';
 import LoadingAnimation from '../../../shared/LoadingAnimation';
 import Icon from '../../../shared/Icon';
 import PermitsMap from './PermitsMap';
 import TypePuck from '../trc/TypePuck';
-import { IM_FOLDER, IM_HOME2 } from '../../../shared/iconConstants';
 import { trcProjectTypes } from '../utils';
-import { getZoningLink } from '../../address/zoning';
+import { permitFieldFormats } from './permitFieldFormats';
 
 // Make query based on URL, render sub components depending on query results
 
@@ -41,175 +39,32 @@ const GET_PERMIT = gql`
   }
 `;
 
-const projectDetails = [
-  {
-    "Accela Label": "Permit Subtype",
-    "Display Label": "Type of permit review",
-    "Description": "Level of project review",
-    "Examples": ""
-  },
-  {
-    "Accela Label": "Plans Folder Location",
-    "Display Label": <span><Icon path={IM_FOLDER} viewBox="0 0 1792 1792"/> Site plans and documents</span>,
-    "Description": "Google Drive folder link to view documents",
-    "Examples": ""
-  },
-  {
-    "Accela Label": "Subdivision Number",
-    "Display Label": "Number of lots to be created",
-    "Description": "Number of subdivision lots to be created",
-    "Examples": ""
-  },
-  {
-    "Accela Label": "Total property size",
-    "Display Label": "Total property size",
-    "Description": "",
-    "Examples": ""
-  },
-  {
-    "Accela Label": "Affordable Housing",
-    "Display Label": "Affordable housing proposed",
-    "Description": "",
-    "Examples": ""
-  },
-  {
-    "Accela Label": "Number of Units",
-    "Display Label": <span><Icon path={IM_HOME2}/>  Number of residential units</span>,
-    "Description": "",
-    "Examples": ""
-  },
-];
-
-const zoningDetails = [
-  {
-    "Accela Label": "Zoning District",
-    "Display Label": "Zoning district",
-    "Description": "",
-    "Examples": ""
-  },
-  {
-    // This is constructed below, not pulled from Accela directly
-    "Accela Label": "setbacks",
-    "Display Label": "Setbacks",
-    "Description": "Minimum distance required between the side or corner property line and structures",
-    "Examples": ""
-  },
-  {
-    "Accela Label": "DTDR Overlay",
-    "Display Label": "Central business district",
-    "Description": "",
-    "Examples": ""
-  },
-  {
-    "Accela Label": "HRC Overlay",
-    "Display Label": "Historic district",
-    "Description": "",
-    "Examples": ""
-  },
-  {
-    "Accela Label": "River District",
-    "Display Label": "River district",
-    "Description": "",
-    "Examples": ""
-  },
-  {
-    "Accela Label": "Landmark",
-    "Display Label": "Historic landmark",
-    "Description": "",
-    "Examples": ""
-  },
-];
-
-const environmentDetails = [
-  {
-    "Accela Label": "Aquatic Buffer",
-    "Display Label": "Aquatic buffer located on this property",
-    "Description": "",
-    "Examples": ""
-  },
-  {
-    "Accela Label": "Flood Plain",
-    "Display Label": "Located in the flood plain",
-    "Description": "",
-    "Examples": ""
-  },
-  {
-    "Accela Label": "Percent Slope",
-    "Display Label": "Average slope of the property",
-    "Description": "",
-    "Examples": ""
-  },
-  {
-    "Accela Label": "Max Elevation",
-    "Display Label": "Maximum elevation of the property",
-    "Description": "",
-    "Examples": ""
-  },
-  {
-    "Accela Label": "Seeking LEED Certification",
-    "Display Label": "Seeking LEED Certification",
-    "Description": "",
-    "Examples": ""
-  }
-];
-
-const dateFormatter = inputDate => moment(new Date(inputDate)).format('MM/DD/YYYY');
-const fieldFormatters = {
-  applied_date: dateFormatter,
-  status_date: dateFormatter,
-  plans_folder_location: d => (<a href={d} target="_blank" rel="noopener noreferrer">Documents folder</a>),
-  zoning_district: d => d.split(',').filter(z => z.length > 0).map((zone, i) => (
-    <a
-      href={getZoningLink(zone) || undefined}
-      target="_blank"
-      rel="noopener noreferrer"
-      key={zone}
-    >
-      {`${i > 0 ? ', ' : ''}${zone}`}
-    </a>
-  )),
-  percent_slope: d => `${Math.round(+d)}%`,
-  max_elevation: d => `${Math.round(+d)} feet`,
-  total_property_size: d => `${d} acres`,
-  permit_subtype: (d, permit) => permit.trcType ? permit.trcType.id : d,
-  dtdr_overlay: d => d === 'No' ? null : d,
-  hrc_overlay: d => d === 'No' ? null : d,
-  river_district: d => d === 'No' ? null : d,
-  landmark: d => d === 'No' ? null : d,
-  aquatic_buffer: d => d === 'No' ? null : d,
-  flood_plain: (d) => {
-    if (d[0] === 'X') {
-      return null;
-    } else if (d === 'AE (100 Year Flood)') {
-      return 'Floodplain located on this property';
-    } else if (d === 'Floodway') {
-      return 'Floodway and floodplain located on this property';
-    }
-    return d;
-  },
-  seeking_leed_certification: d => d === 'No' ? null : d,
-  setbacks: d => d.length > 0 ? d.join(', ') : null,
-  affordable_housing: d => d === 'No' ? null : d,
-};
+// const fieldFormatters = {
+//   applied_date: dateFormatter,
+//   status_date: dateFormatter,
+// };
 
 // TODO: RETURN NULL IF THERE ISN'T A VALUE?  OR LEAVE IT BLANK?
 const PermitDataSubset = props => (
   <div className="detailsFieldset__details-listings">
-    {props.detailsSet.map(d => {
-      const snakeCaseAccelaLabel = d['Accela Label'].toLowerCase().split(' ').join('_');
+    {permitFieldFormats.filter(field => field.displayGroup === props.detailsSet).map(d => {
+      const snakeCaseAccelaLabel = d.accelaLabel.toLowerCase().split(' ').join('_');
       const val =  props.formattedPermit[snakeCaseAccelaLabel];
       if (!val) {
         return;
       }
-      const formattedDisplayVal = fieldFormatters[snakeCaseAccelaLabel] ?   fieldFormatters[snakeCaseAccelaLabel](val, props.formattedPermit) : val;
+      const formattedDisplayVal = d.formatFunc ?   d.formatFunc(val, props.formattedPermit) : val;
       if (!formattedDisplayVal) {
         // Format functions return null if it should not show
         return;
       }
-      return (<div className="form-group form-group--has-content" key={d['Accela Label']}>
+      if (!d.displayLabel) {
+        return formattedDisplayVal;
+      }
+      return (<div className="form-group form-group--has-content" key={d.accelaLabel}>
         <div className="form-group__inner">
           <div className="form-group__label" style={{ fontWeight: 'bold' }}>
-            {d['Display Label']}
+            {d.displayLabel}
           </div>
           <div className="form-group__value">
             {formattedDisplayVal}
@@ -289,7 +144,7 @@ const Permit = props => (
             <div className="detailsFieldset__details-listings">
               <p>{formattedPermit.permit_description}</p>
             </div>
-            <PermitDataSubset detailsSet={projectDetails} formattedPermit={formattedPermit} />
+            <PermitDataSubset detailsSet={'project details'} formattedPermit={formattedPermit} />
             {trcType !== undefined &&
               (<div style={{ display: 'flex' }}>
                 <a style={{ marginRight: '1em' }} href="/development/major">
@@ -311,12 +166,12 @@ const Permit = props => (
         </div>*/}
         <div className="col-sm-12 col-md-6">
           <h2>Zoning Details</h2>
-          <PermitDataSubset detailsSet={zoningDetails} formattedPermit={formattedPermit} />
+          <PermitDataSubset detailsSet={'zoning details'} formattedPermit={formattedPermit} />
         </div>
 
         <div className="col-sm-12 col-md-6">
           <h2>Environmental Details</h2>
-          <PermitDataSubset detailsSet={environmentDetails} formattedPermit={formattedPermit} />
+          <PermitDataSubset detailsSet={'environment details'} formattedPermit={formattedPermit} />
         </div>
       </div>);
     }}
---
+++ src/app/development/permits/permitFieldFormats.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import moment from 'moment';
+import { getZoningLink } from '../../address/zoning';
+import { IM_FOLDER, IM_HOME2 } from '../../../shared/iconConstants';
+// TODO: IMPORT ICONS AND ASSIGN THEM
+
+const dateFormatter = inputDate => moment(new Date(inputDate)).format('MM/DD/YYYY');
+
+export const permitFieldFormats = [
+  {
+    accelaLabel: 'Aquatic Buffer',
+    displayGroup: 'environment details',
+    displayLabel: null,
+    formatFunc: d => d === 'No' ? null : 'Aquatic buffer located on this property',
+  },
+  {
+    accelaLabel: 'Flood Plain',
+    displayGroup: 'environment details',
+    displayLabel: null,
+    formatFunc: (d) => {
+      if (d[0] === 'X') {
+        return null;
+      } else if (d === 'AE (100 Year Flood)') {
+        return 'Floodplain located on this property';
+      } else if (d === 'Floodway') {
+        return 'Floodway and floodplain located on this property';
+      }
+      // If it isn't no
+      return 'Floodplain located on this property';
+    },
+  },
+  {
+    accelaLabel: 'Percent Slope',
+    displayGroup: 'environment details',
+    displayLabel: 'Average slope of the property',
+    formatFunc: d => `${Math.round(+d)}%`,
+  },
+  {
+    accelaLabel: 'Max Elevation',
+    displayGroup: 'environment details',
+    displayLabel: 'Maximum elevation of the property',
+    formatFunc: d => `${Math.round(+d)} feet`,
+  },
+  {
+    accelaLabel: 'Seeking LEED Certification',
+    displayGroup: 'environment details',
+    displayLabel: null,
+    formatFunc: d => d === 'No' ? null : 'Seeking LEED certification',
+  },
+  {
+    accelaLabel: 'Permit Subtype',
+    displayGroup: 'project details',
+    displayLabel: 'Type of permit review',
+    formatFunc: (d, permit) => permit.trcType ? permit.trcType.id : d,
+  },
+  {
+    accelaLabel: 'Plans Folder Location',
+    displayGroup: 'project details',
+    displayLabel: 'Site plans and documents',
+    formatFunc: d => (<a href={d} target='_blank' rel='noopener noreferrer'>Documents folder</a>),
+    description: 'Google Drive folder link to view documents',
+  },
+  {
+    accelaLabel: 'Subdivision Number',
+    displayGroup: 'project details',
+    displayLabel: 'Number of lots to be created',
+    formatFunc: d => `${d} lots`,
+  },
+  {
+    accelaLabel: 'Total Property Size',
+    displayGroup: 'project details',
+    displayLabel: 'Total property size',
+    formatFunc: d => `${d} acres`,
+  },
+  {
+    accelaLabel: 'Affordable Housing',
+    displayGroup: 'project details',
+    displayLabel: null,
+    formatFunc: d => d === 'No' ? null : 'Affordable housing proposed',
+  },
+  {
+    accelaLabel: 'Number of Units',
+    displayGroup: 'project details',
+    displayLabel: 'Number of residential units',
+    formatFunc: d => `${d} units`,
+  },
+  {
+    accelaLabel: 'Permit Number',
+    displayGroup: 'project details',
+    displayLabel: 'Record ID',
+  },
+  {
+    accelaLabel: 'Zoning District',
+    displayGroup: 'zoning details',
+    displayLabel: 'Zoning district',
+    formatFunc: d => d.split(',').filter(z => z.length > 0).map((zone, i) => (
+      <a
+        href={getZoningLink(zone) || undefined}
+        target='_blank'
+        rel='noopener noreferrer'
+        key={zone}
+      >
+        {`${i > 0 ? ', ' : ''}${zone}`}
+      </a>
+    )),
+  },
+  {
+    // This is a combined field from corner, side, and rear-- see formatted permit transoformations in render func
+    accelaLabel: 'Setbacks',
+    displayGroup: 'zoning details',
+    displayLabel: 'Setbacks',
+    formatFunc: d => d.length > 0 ? d.join(', ') : null,
+    description: 'Minimum distance required between the front, rear, side, or corner property line and structures',
+  },
+  {
+    accelaLabel: 'DTDR Overlay',
+    displayGroup: 'zoning details',
+    displayLabel: null,
+    formatFunc: d => d === 'No' ? null : 'Central business district',
+  },
+  {
+    accelaLabel: 'HRC Overlay',
+    displayGroup: 'zoning details',
+    displayLabel: null,
+    formatFunc: d => d === 'No' ? null : 'Historic district',
+  },
+  {
+    accelaLabel: 'River District',
+    displayGroup: 'zoning details',
+    displayLabel: null,
+    formatFunc: d => d === 'No' ? null : 'River district',
+  },
+  {
+    accelaLabel: 'Landmark',
+    displayGroup: 'zoning details',
+    displayLabel: null,
+    formatFunc: d => d === 'No' ? null : 'Historic landmark',
+  }
+];

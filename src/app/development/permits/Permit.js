c6fe03cf885d05ed95627d9bb3893e045618b0c8
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-05-20 13:02:54 -0400

    Lint

--- src/app/development/permits/Permit.js
+++ src/app/development/permits/Permit.js
@@ -44,35 +44,34 @@ const GET_PERMIT = gql`
 
 const PermitDataSubset = props => (
   <div>
-    {permitFieldFormats.filter(field => field.displayGroup === props.detailsSet).sort((a, b) => !a.displayLabel ? -1 : 0).map(d => {
-      const snakeCaseAccelaLabel = d.accelaLabel.toLowerCase().split(' ').join('_');
-      const val =  props.formattedPermit[snakeCaseAccelaLabel];
-      if (!val) {
-        return;
-      }
-      const formattedDisplayVal = d.formatFunc ?   d.formatFunc(val, props.formattedPermit) : val;
-      if (!formattedDisplayVal) {
-        // Format functions return null if it should not show
-        return;
-      }
-      if (!d.displayLabel) {
-        return <div className="permit-form-group bool" key={d.accelaLabel}>{formattedDisplayVal}</div>;
-      }
-      return (<div className="permit-form-group">
-        <span className="display-label">{d.displayLabel}:</span>
-        <span className="formatted-val">{formattedDisplayVal}</span>
-      </div>);
-      // return (<div className="form-group form-group--has-content" key={d.accelaLabel}>
-      //   <div className="form-group__inner">
-      //     <div className="form-group__label" style={{ fontWeight: 'bold' }}>
-      //       {d.displayLabel}
-      //     </div>
-      //     <div className="form-group__value">
-      //       {formattedDisplayVal}
-      //     </div>
-      //   </div>
-      // </div>)
-    })}
+    {permitFieldFormats.filter(field => field.displayGroup === props.detailsSet)
+      .sort(a => (!a.displayLabel ? -1 : 0))
+      .map((d) => {
+        const snakeCaseAccelaLabel = d.accelaLabel.toLowerCase().split(' ').join('_');
+        const val =  props.formattedPermit[snakeCaseAccelaLabel];
+        if (!val) {
+          return null;
+        }
+        const formattedDisplayVal = d.formatFunc ? d.formatFunc(val, props.formattedPermit) : val;
+        if (!formattedDisplayVal) {
+          // Format functions return null if it should not show
+          return null;
+        }
+        if (!d.displayLabel) {
+          return (
+            <div className="permit-form-group bool" key={d.accelaLabel}>
+              {formattedDisplayVal}
+            </div>
+          );
+        }
+        return (
+          <div className="permit-form-group">
+            <span className="display-label">{d.displayLabel}:</span>
+            <span className="formatted-val">{formattedDisplayVal}</span>
+          </div>
+        );
+      })
+    }
   </div>
 );
 
@@ -94,14 +93,13 @@ const Permit = props => (
       }
 
       const thisPermit = data.permits[0];
-      let trcType = undefined;
+      let trcType;
       if (thisPermit.permit_group === 'Planning') {
         trcType = Object.values(trcProjectTypes).find(type =>
           type.permit_type === thisPermit.permit_type &&
-          type.permit_subtype === thisPermit.permit_subtype
-        )
+          type.permit_subtype === thisPermit.permit_subtype);
       }
-      let formattedPermit = Object.assign({}, thisPermit, { trcType: trcType });
+      const formattedPermit = Object.assign({}, thisPermit, { trcType });
       // These are all the "misc" info fields that may or may not be filled out for any permit
       thisPermit.custom_fields.forEach((customField) => {
         formattedPermit[customField.name.toLowerCase().split(' ').join('_')] = customField.value;
@@ -129,49 +127,50 @@ const Permit = props => (
       // Don't show map if there are no coordinates
       const showMap = thisPermit.y && thisPermit.x;
 
-      return (<div className="container">
-        <div className="row">
-          <h1 className="title__text">{formattedPermit.application_name}</h1>
-          {showMap && (<div className="col-sm-12 col-md-6">
-            <div className="map-container" style={{ height: `75vh` }}>
-              <PermitsMap
-                permitData={mapData}
-                centerCoords={[formattedPermit.y, formattedPermit.x]}
-                zoom={14}
-              />
-            </div>
-          </div>)}
-          <div className={`col-sm-12 col-md-${showMap ? 6 : 12} permit-details-card`}>
-            <p id="permit-description">{formattedPermit.permit_description}</p>
-            <PermitDataSubset detailsSet={'project details'} formattedPermit={formattedPermit} />
-            {trcType !== undefined &&
-              (<div style={{ display: 'flex', marginTop: '1rem' }}>
-                <a style={{ marginRight: '1em' }} href="/development/major">
-                  <TypePuck
-                    typeObject={trcType}
+      return (
+        <div className="container">
+          <div className="row">
+            <h1 className="title__text">{formattedPermit.application_name}</h1>
+            {showMap && (
+              <div className="col-sm-12 col-md-6">
+                <div className="map-container" style={{ height: '75vh' }}>
+                  <PermitsMap
+                    permitData={mapData}
+                    centerCoords={[formattedPermit.y, formattedPermit.x]}
+                    zoom={14}
                   />
-                </a>
-                <p><em>
-                  This is a major development.  <a href="/development/major">Learn more</a> about the large-scale development process in Asheville.
-                </em></p>
-              </div>)
-            }
+                </div>
+              </div>
+            )}
+            <div className={`col-sm-12 col-md-${showMap ? 6 : 12} permit-details-card`}>
+              <p id="permit-description">{formattedPermit.permit_description}</p>
+              <PermitDataSubset detailsSet="project details" formattedPermit={formattedPermit} />
+              {trcType !== undefined && (
+                <div style={{ display: 'flex', marginTop: '1rem' }}>
+                  <a style={{ marginRight: '1em' }} href="/development/major">
+                    <TypePuck
+                      typeObject={trcType}
+                    />
+                  </a>
+                  <p>
+                    <em>
+                      This is a major development.  <a href="/development/major">Learn more</a> about the large-scale development process in Asheville.
+                    </em>
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-6 permit-details-card">
+            <h2>Zoning Details</h2>
+            <PermitDataSubset detailsSet="zoning details" formattedPermit={formattedPermit} />
+          </div>
+          <div className="col-sm-12 col-md-6 permit-details-card">
+            <h2>Environmental Details</h2>
+            <PermitDataSubset detailsSet="environment details" formattedPermit={formattedPermit} />
           </div>
         </div>
-        {/*<div className="row">
-          <h2>Timeline</h2>
-          <PermitTasks {...props} />
-        </div>*/}
-        <div className="col-sm-12 col-md-6 permit-details-card">
-          <h2>Zoning Details</h2>
-          <PermitDataSubset detailsSet={'zoning details'} formattedPermit={formattedPermit} />
-        </div>
-
-        <div className="col-sm-12 col-md-6 permit-details-card">
-          <h2>Environmental Details</h2>
-          <PermitDataSubset detailsSet={'environment details'} formattedPermit={formattedPermit} />
-        </div>
-      </div>);
+      );
     }}
   </Query>
 );

112835bcbfe6921ec58b7470385b078d7b43ca7b
Frances Ruiz <frances.ruiz@gmail.com>
2019-08-02 11:32:25 -0400

    building construction icon

--- src/app/capital_projects/ProjectsTable.js
+++ src/app/capital_projects/ProjectsTable.js
@@ -11,6 +11,7 @@ import {
   IM_BUS,
   LI_BOLD,
   IM_DROPLET,
+  FA_HAMMER,
 } from '../../shared/iconConstants';
 import expandingRows from '../../shared/react_table_hoc/ExpandingRows';
 import createFilterRenderer from '../../shared/FilterRenderer';
@@ -36,6 +37,8 @@ const getIcon = (category, isExpanded) => {
       return <Icon path={IM_SHIELD3} size={25} color={isExpanded ? '#fff' : '#4077a5'} />;
     case 'Water':
       return <Icon path={IM_DROPLET} size={25} color={isExpanded ? '#fff' : '#4077a5'} />;
+    case 'Building Construction':
+      return <Icon path={FA_HAMMER} size={25} color={isExpanded ? '#fff' : '#4077a5'} />;
     default:
       return <svg xmlns="http://www.w3.org/2000/svg" height="25px" transform="translate(0,4)" version="1.1" viewBox="0 0 16 16" width="25px"><g fill="none" fillRule="evenodd" id="Icons with numbers" stroke="none" strokeWidth="1"><g fill={isExpanded ? '#fff' : '#4077a5'} id="Group" transform="translate(-528.000000, -576.000000)"><path d="M536,592 C531.581722,592 528,588.418278 528,584 C528,579.581722 531.581722,576 536,576 C540.418278,576 544,579.581722 544,584 C544,588.418278 540.418278,592 536,592 Z M541,586 C542.10457,586 543,585.10457 543,584 C543,582.89543 542.10457,582 541,582 C539.89543,582 539,582.89543 539,584 C539,585.10457 539.89543,586 541,586 Z M531,586 C532.10457,586 533,585.10457 533,584 C533,582.89543 532.10457,582 531,582 C529.89543,582 529,582.89543 529,584 C529,585.10457 529.89543,586 531,586 Z M536,586 C537.10457,586 538,585.10457 538,584 C538,582.89543 537.10457,582 536,582 C534.89543,582 534,582.89543 534,584 C534,585.10457 534.89543,586 536,586 Z M536,586" id="Oval 12 copy" /></g></g></svg>;
   }

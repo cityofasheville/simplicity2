099e27067e5e8219632491c6dcdb3b1f54e937f5
Jon Wilson <jtwilson@ashevillenc.gov>
2019-10-31 14:12:31 -0400

    audit vulns; removed Google map option which wasn't working anyway

--- src/app/development/permits/PermitsTableWrapper.js
+++ src/app/development/permits/PermitsTableWrapper.js
@@ -75,7 +75,7 @@ const PermitsTableWrapper = props => (
         <div>
           <div className="map-container" style={{ height: '350px', width: '100%' }}>
             <a
-              style={{ left: '60px', top: '60px', right: '60px' }}
+              style={{ left: '60px', right: '60px' }}
               href="#permitsDataTable"
               className="skip-nav-link"
               onClick={() => { document.getElementById('permitsDataTable').focus(); }}

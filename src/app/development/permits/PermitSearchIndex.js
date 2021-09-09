21c52a03f0d853449a3bbd34d0de4712445188a6
Jon Wilson <jtwilson@ashevillenc.gov>
2021-09-09 12:51:46 -0400

    Revert "link to search"
    This reverts commit 0fd2a56a677dae8369626c5aeee5b0bf5c23cd56.

--- src/app/development/permits/PermitSearchIndex.js
+++ src/app/development/permits/PermitSearchIndex.js
@@ -13,13 +13,6 @@ class PermitSearchIndex extends React.Component {
       <div>
         <h1 className="">Look Up an Existing Application</h1>
         <PermitSearchBar />
-        <div style={{marginTop: "40px"}}>
-          <h4>If you don't know the Application ID, you can look it up here: 
-          </h4>
-          <div style={{fontSize: "1.5em"}}>
-          <a href="https://simplicity.ashevillenc.gov/permits">Search Map by Date</a>
-          </div>
-        </div>
       </div>
     );
   }

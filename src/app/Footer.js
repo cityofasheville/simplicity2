f9afd48c39b588325685f658bfceadfeb6f612de
6foot5 <6foot5@gmail.com>
2021-09-15 16:23:46 -0400

    update feedback link, add permit detail links to map cards and table items

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -8,7 +8,15 @@ const Footer = () => (
     <footer className="footer">
       <div className="container">
         <div className="col-sm-12">
-          <div style={{ fontStyle: 'italic' }} className="text-center">We strive for full accessibility. Report issues with this website by emailing <a className="inText" href="mainlto:help@ashevillenc.gov" target="_blank" rel="noopener noreferrer" title="helpdesk email">our helpdesk</a>.
+          <div style={{ fontStyle: 'italic' }} className="text-center">
+            We strive for full accessibility. Report issues with this website through our  
+            <a className="inText" 
+              href="https://docs.google.com/forms/d/e/1FAIpQLSdjNwOmoDY3PjQOVreeSL07zgI8otIIPWjY7BnejWMAjci8-w/viewform" 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              title="Provide Feedback"
+              style={{ marginLeft: '4px' }}>feedback form
+            </a>.
           </div>
           <div className="text-center" style={{ fontStyle: 'italic', marginBottom: '2px' }}>It&apos;s open source! Fork it on <a href="https://github.com/cityofasheville/simplicity2" target="_blank">GitHub <Icon path={IM_GITHUB} size={23} /></a></div>
         </div>

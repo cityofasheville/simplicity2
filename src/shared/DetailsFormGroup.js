2573e1f98237632b365c0642fee77fb61230946e
Berenice Vasquez <bvprogramit@gmail.com>
2021-12-07 13:55:03 -0500

    Added button to get ss data

--- src/shared/DetailsFormGroup.js
+++ src/shared/DetailsFormGroup.js
@@ -4,12 +4,15 @@ import PropTypes from 'prop-types';
 const DetailsFormGroup = props => (
   <div className={props.icon ? 'form-group form-group--has-content form-group--has-icon' : 'form-group form-group--has-content'}>
     <div className="form-group__inner">
-      <div className="form-group__label">
+      <div className={props.name == "pinnum" ? 'form-group__label steep-slope-tag' : 'form-group__label'}>
       {props.icon !== null &&
       <span>{props.icon}</span>
       }
       {props.hasLabel &&
         <label htmlFor={props.name}>{props.label}</label>
+      }
+      { props.name == "pinnum" && 
+      <div className="steep-slope-btn">Get Steep Slope Data</div>
       }
       </div>
       <div className="form-group__value" name={props.name}>{props.value}</div>

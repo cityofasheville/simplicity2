2bf6681d50ddc3ffc78f001387d6b61893418aa8
bvprogramit <35052396+bvprogramit@users.noreply.github.com>
2021-12-15 11:39:57 -0500

    Prod deploy 12.15.21 (#595)
    * Updated Readme (#585)
    * Fixed budget link (#586)
    * Removed budget option and added white space above the row (#588)
    * Removed budget references (#589)
    * Fixed NaN (#590)
    * Added steep slope data (#593)
    * Added new card for Steep Slope Data (#594)

--- src/app/property/Property.js
+++ src/app/property/Property.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'react-apollo';
 import AccessibleReactTable, { CellFocusWrapper } from 'accessible-react-table';
 import gql from 'graphql-tag';
@@ -29,6 +29,26 @@ import {
 } from '../../shared/iconConstants';
 import createFilterRenderer from '../../shared/FilterRenderer';
 
+const getSteepSlope = (pinValue, callback) => {
+  let steepSlopeUrl = "https://mapwnc.org/api/slopebypin/" + pinValue;
+  fetch(steepSlopeUrl)
+  .then(response => response.json())
+  .then(data => {
+    callback(true);
+    document.getElementById('ssData').classList.remove('hide-elem');
+    document.getElementById('successData').classList.remove('hide-elem');
+    document.getElementById('jurisdiction').innerText = data.jurisdiction;
+    document.getElementById('acres').innerText = data.acres;
+    document.getElementById('elevation').innerText = data.maxElevation;
+    document.getElementById('percentSlope').innerText = data.percentSlope;
+  })
+  .catch((e) => {
+    document.getElementById('ssData').classList.remove('hide-elem');
+    document.getElementById('slopeError').classList.remove('hide-elem');
+    document.getElementById('slopeError').innerText = "There has been an error in the server, please try again.";
+  });
+}
+
 const getDollars = (value) => {
   const initialSymbols = value < 0 ? '-$' : '$';
   return [initialSymbols, Math.abs(value).toLocaleString()].join('');
@@ -43,6 +63,8 @@ const Property = (props) => {
   if (props.data.error) {
     return <Error message={props.data.error.message} />;
   }
+
+  const [isSlopeDataShown, setSlopeData] = useState(false);
 
   const propertyData = props.inTable ? props.data : props.data.properties[0];
   const dataForAddressesTable = [];
@@ -330,6 +352,35 @@ const Property = (props) => {
                               icon={<Icon path={IM_LIBRARY} size={20} />}
                             />
                         }
+                      </div>
+                    }
+                    hasLabel
+                  />
+                  <DetailsFormGroup
+                    label="Steep Slope"
+                    name="steepslope"
+                    colWidth="6"
+                    value={
+                      <div>
+                        <div className='steep-slope-tag'>
+                            {
+                              !isSlopeDataShown && 
+                              <button className="btn btn-default steep-slope-btn"
+                                onClick={()=>getSteepSlope(propertyData.pinnum, setSlopeData)}
+                                title="Get Steep Slope" aria-label="get Steep Slope Data">
+                                <span>Get Steep Slope Data</span>
+                              </button>
+                            }
+                        </div>
+                        <div id="ssData" className="detailsFieldset__details-listings hide-elem">
+                          <div id="successData" className="ss-container hide-elem" aria-label="Slope Steep Data">
+                            <div><p className="tag">Jurisdiction:</p><p id="jurisdiction" className="info"></p></div>
+                            <div><p className="tag">Acres: </p> <p id="acres" className="info"></p></div>
+                            <div><p className="tag">Maximum Elevation:</p><p id="elevation" className="info"></p></div>
+                            <div><p className="tag">Percent Slope:</p><p id="percentSlope" className="info"></p></div>
+                          </div>
+                          <div id="slopeError" aria-label="Server Error" className="slope-error hide-elem"></div>
+                        </div>
                       </div>
                     }
                     hasLabel

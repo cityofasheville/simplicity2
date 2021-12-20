a0c47f7c508348641ae0a92ae422475bb8aa24dd
Berenice Vasquez <bvprogramit@gmail.com>
2021-12-20 16:55:16 -0500

    Added spinner.

--- src/app/property/SteepSlope.js
+++ src/app/property/SteepSlope.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useState } from 'react';
+import LoadingAnimation from '../../shared/LoadingAnimation';
 
 const SteepSlope = (props) => {
     const [isSlopeDataShown, setSlopeData] = useState(false);
     const [isSlopeDataError, setSlopeDataError] = useState(false);
+    const [dataLoaded, setDataLoaded] = useState(false);
     const [showSSData, setShowSSData] = useState(
         {
             jurisdiction: "",
@@ -14,23 +16,30 @@ const SteepSlope = (props) => {
     );
 
     const getSteepSlope = (pinValue, setSlopeData, setShowSSData, setSlopeDataError) => {
-        let steepSlopeUrl = "https://mapwnc.org/api/slopebypin/" + pinValue;
+        setSlopeData(true);
+        setSlopeDataError(false);
+        let steepSlopeUrl = "https://mapwnc.org/api/slopebypin2/" + pinValue;
         fetch(steepSlopeUrl)
         .then(response => response.json())
         .then(data => {
-            setSlopeData(true);
             setShowSSData(data);
+            setDataLoaded(true);
         })
         .catch((e) => {
+            setSlopeData(false);
             setSlopeDataError(true);
         });
     }
 
     return (
         <div>
+            {
+                (isSlopeDataShown && !dataLoaded && !isSlopeDataError) && 
+                <LoadingAnimation />
+            }
             <div className='steep-slope-tag'>
                 {
-                    !isSlopeDataShown && 
+                    (!isSlopeDataShown && !dataLoaded) &&
                     <button className="btn btn-default steep-slope-btn"
                     onClick={()=>getSteepSlope(props.pinnum, setSlopeData, setShowSSData, setSlopeDataError)}
                     title="Get Steep Slope" aria-label="get Steep Slope Data">
@@ -38,8 +47,8 @@ const SteepSlope = (props) => {
                     </button>
                 }
             </div>
-            <div id="ssData" className={(isSlopeDataShown || isSlopeDataError) ? 'detailsFieldset__details-listings' : 'detailsFieldset__details-listings hide-elem'}>
-                <div id="successData" className={(!isSlopeDataError && isSlopeDataShown) ? 'ss-container' : 'ss-container hide-elem' } aria-label="Slope Steep Data">
+            <div id="ssData" className={((isSlopeDataShown && dataLoaded) || isSlopeDataError) ? 'detailsFieldset__details-listings' : 'detailsFieldset__details-listings hide-elem'}>
+                <div id="successData" className={(!isSlopeDataError && isSlopeDataShown && dataLoaded) ? 'ss-container' : 'ss-container hide-elem' } aria-label="Slope Steep Data">
                 <div><p className="tag">Jurisdiction:</p><p className="info">{showSSData.jurisdiction ? showSSData.jurisdiction : '--'}</p></div>
                 <div><p className="tag">Acres: </p> <p className="info">{showSSData.acres  ? showSSData.acres : '--'}</p></div>
                 <div><p className="tag">Maximum Elevation:</p><p className="info">{showSSData.maxElevation ? showSSData.maxElevation : '--'}</p></div>

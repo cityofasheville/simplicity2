5e0dc548effbd13d2c7ec679fbad534c0ed0ee2a
Berenice Vasquez <bvprogramit@gmail.com>
2022-01-06 09:16:18 -0500

    Added back the CJ card

--- src/app/address/Address.js
+++ src/app/address/Address.js
@@ -396,14 +396,6 @@ const Address = props => (
                     icon={<Icon path={IM_LOCATION2} size={20} />
                     }
                   />
-                  {/* <DetailsFormGroup 
-                    label="Climate Justice"
-                    name="climate"
-                    value={
-                      <ClimateJustice civicAddress={props.location.query.id.trim()} pinnum={0} />
-                    }
-                    hasLabel
-                  /> */}
                   <DetailsIconLinkFormGroup
                     label={content.property_information}
                     title={content.property_information}
@@ -413,6 +405,14 @@ const Address = props => (
                   />
                 </div>
               </fieldset>
+              <DetailsFormGroup 
+                label="Climate Justice"
+                name="climate"
+                value={
+                  <ClimateJustice civicAddress={props.location.query.id.trim()} pinnum={0} />
+                }
+                hasLabel
+              />
             </div>
           </div>
         </div>

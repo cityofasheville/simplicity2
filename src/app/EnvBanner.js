fe5d05ae052889c35d29d36896a25d99c68c6ac1
Rick Barley <6foot5@gmail.com>
2022-08-22 17:02:44 -0400

    add page path to production link in env banner alert

--- src/app/EnvBanner.js
+++ src/app/EnvBanner.js
@@ -10,19 +10,27 @@ const EnvBanner = props => {
     bannerOverride = true;
   }
 
-  console.log();
-
   if (window.location.href.indexOf('simplicity.ashevillenc.gov') === -1 && !bannerOverride) {
+
+    let productionPathAddons = '';
 
     if (window.location.href.indexOf('development/major') > -1) {
       defaultLeftMargin = 200;
     }
 
+    if (window.location.pathname.length) {
+      productionPathAddons += window.location.pathname;
+    }
+
+    if (window.location.search.length) {
+      productionPathAddons += window.location.search;
+    }
+
     bannerContent = (
       <div role={'alert'} className='alert' style={{backgroundColor: 'orange', marginTop: '32px', marginLeft: defaultLeftMargin}}>
         <p style={{fontWeight: '500', fontSize: '1.5rem', textAlign:'center'}}><em>Attention!</em></p> <p>This is an experimental version of SimpliCity, and may
-        produce unexpected results. Unless you understand why you've ended up here, you should probably visit the 
-        <a href="https://simplicity.ashevillenc.gov">production version of SimpliCity</a> instead. </p>
+        produce unexpected results. Unless you understand how you've ended up here, we
+        recommend <a href={`https://simplicity.ashevillenc.gov${productionPathAddons}`} style={{color: 'black'}}>visiting this page in the production version of SimpliCity</a> instead. </p>
       </div>
     );
   }

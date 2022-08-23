7458668e951c05c68f2eb0beb148457c362b12b9
6foot5 <34613036+6foot5@users.noreply.github.com>
2022-08-23 10:00:29 -0400

    Rb/non prod banner (#643)
    * add banner to non-prod environments
    
    * add page path to production link in env banner alert
    
    * add newline at eof for edited dotfiles

---
+++ src/app/EnvBanner.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const EnvBanner = props => {
+
+  let bannerContent = '';
+  let bannerOverride = false;
+  let defaultLeftMargin = 0;
+
+  if (typeof process.env.REACT_APP_SUPPRESS_ENV_WARNING !== "undefined" && +process.env.REACT_APP_SUPPRESS_ENV_WARNING) {
+    bannerOverride = true;
+  }
+
+  if (window.location.href.indexOf('simplicity.ashevillenc.gov') === -1 && !bannerOverride) {
+
+    let productionPathAddons = '';
+
+    if (window.location.href.indexOf('development/major') > -1) {
+      defaultLeftMargin = 200;
+    }
+
+    if (window.location.pathname.length) {
+      productionPathAddons += window.location.pathname;
+    }
+
+    if (window.location.search.length) {
+      productionPathAddons += window.location.search;
+    }
+
+    bannerContent = (
+      <div role={'alert'} className='alert' style={{backgroundColor: 'orange', marginTop: '32px', marginLeft: defaultLeftMargin}}>
+        <p style={{fontWeight: '500', fontSize: '1.5rem', textAlign:'center'}}><em>Attention!</em></p> <p>This is an experimental version of SimpliCity, and may
+        produce unexpected results. Unless you understand how you've ended up here, we
+        recommend <a href={`https://simplicity.ashevillenc.gov${productionPathAddons}`} style={{color: 'black'}}>visiting this page in the production version of SimpliCity</a> instead. </p>
+      </div>
+    );
+  }
+
+  return (
+    bannerContent
+  );
+} 
+
+export default EnvBanner;

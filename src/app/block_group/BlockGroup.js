9f86e3fd0d46610a38e87c1751b4b711d1c7c932
6foot5 <6foot5@gmail.com>
2023-08-15 17:19:55 -0400

    color code block groups based on climate risk

--- src/app/block_group/BlockGroup.js
+++ src/app/block_group/BlockGroup.js
@@ -1,105 +1,255 @@
-import React from 'react';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
-import LoadingAnimation from '../../shared/LoadingAnimation';
-import Error from '../../shared/Error';
-import { Link } from 'react-router';
-import Icon from '../../shared/Icon';
-import Map from '../../shared/visualization/Map';
-import { IM_USERS } from '../../shared/iconConstants';
-import ButtonGroup from '../../shared/ButtonGroup';
-import LinkButton from '../../shared/LinkButton';
-import PageHeader from '../../shared/PageHeader';
-import { getBoundsFromPolygonData, combinePolygonsFromNeighborhoodList } from '../../utilities/mapUtilities';
+import React from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import LoadingAnimation from "../../shared/LoadingAnimation";
+import Error from "../../shared/Error";
+import { Link } from "react-router";
+import Icon from "../../shared/Icon";
+import Map from "../../shared/visualization/Map";
+import { IM_USERS } from "../../shared/iconConstants";
+import ButtonGroup from "../../shared/ButtonGroup";
+import LinkButton from "../../shared/LinkButton";
+import PageHeader from "../../shared/PageHeader";
+import {
+  getBoundsFromPolygonData,
+  combinePolygonsFromNeighborhoodList,
+} from "../../utilities/mapUtilities";
 
 const BlockGroup = (props) => {
-  if (props.data.loading) { // eslint-disable-line react/prop-types
+  if (props.data.loading) {
+    // eslint-disable-line react/prop-types
     return <LoadingAnimation />;
   }
-  if (props.data.error) { // eslint-disable-line react/prop-types
+  if (props.data.error) {
+    // eslint-disable-line react/prop-types
     return <Error message={props.data.error.message} />; // eslint-disable-line react/prop-types
   }
 
+  let fillColor, scoreColor;
+  if (isNaN(props.data.blockgroups[0].sum_scores)) {
+    fillColor = null;
+    scoreColor = null;
+  } else if (
+    props.data.blockgroups[0].sum_scores > 0 &&
+    props.data.blockgroups[0].sum_scores <= 6
+  ) {
+    fillColor = "#413da1";
+    scoreColor = "white";
+  } else if (
+    props.data.blockgroups[0].sum_scores > 6 &&
+    props.data.blockgroups[0].sum_scores <= 10
+  ) {
+    fillColor = "#9a3abb";
+    scoreColor = "white";
+  } else if (
+    props.data.blockgroups[0].sum_scores > 10 &&
+    props.data.blockgroups[0].sum_scores <= 14
+  ) {
+    fillColor = "#d66f96";
+    scoreColor = "black";
+  } else if (
+    props.data.blockgroups[0].sum_scores > 14 &&
+    props.data.blockgroups[0].sum_scores <= 18
+  ) {
+    fillColor = "#faab6b";
+    scoreColor = "black";
+  } else if (
+    props.data.blockgroups[0].sum_scores > 18 &&
+    props.data.blockgroups[0].sum_scores <= 24
+  ) {
+    fillColor = "#f3fa52";
+    scoreColor = "black";
+  }
+
+  console.log(`fill color: ${fillColor}, ${scoreColor}`);
+
   return (
     <div>
-      <PageHeader h1={props.data.blockgroups[0].name.slice(0, -33)} dataType="Census Block Group in Buncombe County, North Carolina" h2={"About this Block Group"} icon={<Icon path={IM_USERS} size={50} />}>
+      <PageHeader
+        h1={props.data.blockgroups[0].name.slice(0, -33)}
+        dataType="Census Block Group in Buncombe County, North Carolina"
+        h2={"About this Block Group"}
+        icon={<Icon path={IM_USERS} size={50} />}
+      >
         <ButtonGroup alignment="">
           <LinkButton
             pathname="/address"
             query={{
-              id: props.location.query.fromAddress, entities: props.location.query.entities, search: props.location.query.search, hideNavbar: props.location.query.hideNavbar,
-              entity: "address", label: props.location.query.label
-            }
-            }
+              id: props.location.query.fromAddress,
+              entities: props.location.query.entities,
+              search: props.location.query.search,
+              hideNavbar: props.location.query.hideNavbar,
+              entity: "address",
+              label: props.location.query.label,
+            }}
           >
             Back to address
           </LinkButton>
         </ButtonGroup>
       </PageHeader>
-      <div><a href="https://www.census.gov/programs-surveys/geography/about/glossary.html#par_textimage_4" target="_blank">What is a Block Group? (census.gov)</a></div>
+      <div>
+        <a
+          href="https://www.census.gov/programs-surveys/geography/about/glossary.html#par_textimage_4"
+          target="_blank"
+        >
+          What is a Block Group? (census.gov)
+        </a>
+      </div>
       <div className="row small-padding">
         <div className="col-sm-12">
           <fieldset className="detailsFieldset">
             <div className="map-container">
               <Map
                 drawPolygon
-                polygonData={combinePolygonsFromNeighborhoodList([props.data.blockgroups[0]])}
-                bounds={getBoundsFromPolygonData([props.data.blockgroups[0].polygon])}
+                polygonData={combinePolygonsFromNeighborhoodList([
+                  props.data.blockgroups[0],
+                ])}
+                bounds={getBoundsFromPolygonData([
+                  props.data.blockgroups[0].polygon,
+                ])}
+                color={fillColor}
+                opacity={1}
+                weight={3}
+                fillColor={fillColor}
+                fillOpacity={0.5}
               />
             </div>
           </fieldset>
         </div>
         <div className="col-sm-12">
-
           <div className="row small-padding">
             <h2>Climate Justice Data for this Block Group</h2>
-            <h3>Climate Justice Index Score: <span style={{ color: "red", fontWeight: "bold" }}>{props.data.blockgroups[0].sum_scores}</span> out of 25</h3>
+            <h3>
+              Climate Justice Index Score:{" "}
+              <span
+                style={{
+                  backgroundColor: fillColor,
+                  paddingLeft: "12px",
+                  paddingRight: "12px",
+                  // marginRight: "16px",
+                  border: "1px solid gray",
+                  fontWeight: "bold",
+                  color: scoreColor,
+                }}
+              >
+                {props.data.blockgroups[0].sum_scores}
+              </span>{" "}
+              {/* <span style={{ fontWeight: "bold" }}>
+                {props.data.blockgroups[0].sum_scores}
+              </span>{" "} */}
+              out of 25
+            </h3>
             <div className="">
               For mitigation strategies, see the
-              <a href='https://drive.google.com/file/d/0BzZzONRPV-VAVF9vb2pOMUtkRmFJR1AyNFluYU5ESU9rODRJ/view?resourcekey=0-ZQ80xC-a8bw4JDs7z0Neaw' className='' target="_blank"> Climate Resiliency Guide</a>.
-
-              Overview of Asheville's Climate Justice Initiative:
-              <a href='https://storymaps.arcgis.com/stories/1d90d45f3e71482397a944e8d6786df4' className='' target="_blank"> Storymap</a>.
+              <a
+                href="https://drive.google.com/file/d/0BzZzONRPV-VAVF9vb2pOMUtkRmFJR1AyNFluYU5ESU9rODRJ/view?resourcekey=0-ZQ80xC-a8bw4JDs7z0Neaw"
+                className=""
+                target="_blank"
+              >
+                {" "}
+                Climate Resiliency Guide
+              </a>
+              . Overview of Asheville's Climate Justice Initiative:
+              <a
+                href="https://storymaps.arcgis.com/stories/1d90d45f3e71482397a944e8d6786df4"
+                className=""
+                target="_blank"
+              >
+                {" "}
+                Storymap
+              </a>
+              .
             </div>
             <p>
-              Risk factors are scaled from 1 to 5, and summed up to caclulate the Index Score.
+              Risk factors are scaled from 1 to 5, and summed up to caclulate
+              the Index Score.
             </p>
-            <ul className='cjdata'>
-              <li><h4>BIPOC Score: {props.data.blockgroups[0].bipoc_score}</h4>
+            <ul className="cjdata">
+              <li>
+                <h4>BIPOC Score: {props.data.blockgroups[0].bipoc_score}</h4>
                 <ul>
                   <li>BIPOC is Black, Indigenous and People of Color.</li>
-                  <li>This area has a <b>{props.data.blockgroups[0].bipoc}%</b> BIPOC Population</li>
-                </ul>
-              </li>
-              <li><h4>Heat Vulnerability Score: {props.data.blockgroups[0].heat_score}</h4>
-                <ul>
-                  <li>This heat vulnerability index shows which parts of Asheville are vulnerable to extreme heat, taking into account land surface temperature, tree cover, age, and poverty level.</li>
-                  <li>Overall Heat Vulnerability Index Value: <b>{props.data.blockgroups[0].hvi_value.toFixed(2)}</b></li>
-                  <li>The closer this value is to 3, the more vulnerable it is, and the closer it is to 0, the less vulnerable it is.
+                  <li>
+                    This area has a <b>{props.data.blockgroups[0].bipoc}%</b>{" "}
+                    BIPOC Population
                   </li>
                 </ul>
               </li>
-              <li><h4>Energy Burden Score: {props.data.blockgroups[0].energy_sco}</h4>
+              <li>
+                <h4>
+                  Heat Vulnerability Score:{" "}
+                  {props.data.blockgroups[0].heat_score}
+                </h4>
                 <ul>
                   <li>
-                    <p>Energy Burden represents the percentage of annual income that a household pays towards their energy bills (electricity/gas utility.)</p>
+                    This heat vulnerability index shows which parts of Asheville
+                    are vulnerable to extreme heat, taking into account land
+                    surface temperature, tree cover, age, and poverty level.
+                  </li>
+                  <li>
+                    Overall Heat Vulnerability Index Value:{" "}
+                    <b>{props.data.blockgroups[0].hvi_value.toFixed(2)}</b>
+                  </li>
+                  <li>
+                    The closer this value is to 3, the more vulnerable it is,
+                    and the closer it is to 0, the less vulnerable it is.
                   </li>
                 </ul>
               </li>
-              <li><h4>CDC Social Vulnerability Score: {props.data.blockgroups[0].cdc_score}</h4>
+              <li>
+                <h4>
+                  Energy Burden Score: {props.data.blockgroups[0].energy_sco}
+                </h4>
+                <ul>
+                  <li>
+                    <p>
+                      Energy Burden represents the percentage of annual income
+                      that a household pays towards their energy bills
+                      (electricity/gas utility.)
+                    </p>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <h4>
+                  CDC Social Vulnerability Score:{" "}
+                  {props.data.blockgroups[0].cdc_score}
+                </h4>
                 <ul>
                   <li>CDC Social Vulnerability Index</li>
-                  <li>2018 Overall Score: <b>{props.data.blockgroups[0].rpl_themes}</b></li>
-                  <li>Possible scores range from 0 (lowest vulnerability) to 1 (highest vulnerability).
+                  <li>
+                    2018 Overall Score:{" "}
+                    <b>{props.data.blockgroups[0].rpl_themes}</b>
+                  </li>
+                  <li>
+                    Possible scores range from 0 (lowest vulnerability) to 1
+                    (highest vulnerability).
                   </li>
                 </ul>
               </li>
-              <li><h4>Climate Resiliency Neighborhood Threats Score: {props.data.blockgroups[0].resiliency}</h4>
+              <li>
+                <h4>
+                  Climate Resiliency Neighborhood Threats Score:{" "}
+                  {props.data.blockgroups[0].resiliency}
+                </h4>
                 <ul>
-                  <li>The Climate Resiliency Neighborhood Threats Score was determined by taking the percentage of land with risk by neighborhood (census block group) and classifying by risk level.</li>
-                  <li>Score from Flood Risk: <b>{props.data.blockgroups[0].resflood}</b></li>
-                  <li>Score from Wildfire Risk: <b>{props.data.blockgroups[0].wfirescore}</b></li>
-                  <li>Score from Landslide Risk: <b>{props.data.blockgroups[0].resland}</b>
+                  <li>
+                    The Climate Resiliency Neighborhood Threats Score was
+                    determined by taking the percentage of land with risk by
+                    neighborhood (census block group) and classifying by risk
+                    level.
+                  </li>
+                  <li>
+                    Score from Flood Risk:{" "}
+                    <b>{props.data.blockgroups[0].resflood}</b>
+                  </li>
+                  <li>
+                    Score from Wildfire Risk:{" "}
+                    <b>{props.data.blockgroups[0].wfirescore}</b>
+                  </li>
+                  <li>
+                    Score from Landslide Risk:{" "}
+                    <b>{props.data.blockgroups[0].resland}</b>
                   </li>
                 </ul>
               </li>
@@ -107,13 +257,13 @@ const BlockGroup = (props) => {
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
 const getBlockGroupQuery = gql`
   query getBlockGroup($geoid: [String]) {
-    blockgroups (geoid: $geoid) {
+    blockgroups(geoid: $geoid) {
       geoid
       name
       below_pov
@@ -164,7 +314,7 @@ const getBlockGroupQuery = gql`
 `;
 
 const BlockGroupQGL = graphql(getBlockGroupQuery, {
-  options: ownProps => ({
+  options: (ownProps) => ({
     variables: {
       geoid: [ownProps.location.query.id.trim()],
     },
--- src/shared/visualization/Map.js
+++ src/shared/visualization/Map.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
-import L from 'leaflet';
+import React from "react";
+import PropTypes from "prop-types";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import L from "leaflet";
 import {
   Map as LeafletMap,
   Marker,
@@ -12,44 +12,44 @@ import {
   Polyline,
   Polygon,
   LayersControl,
-} from 'react-leaflet';
-import { GoogleLayer } from 'react-leaflet-google';
-import MarkerClusterGroup from 'react-leaflet-markercluster';
-import MapLegendControl from './MapLegendControl';
-import Icon from '../Icon';
-import { IM_LIST2 } from '../iconConstants';
-import { convertPolygonsToLatLngArrays } from '../../utilities/mapUtilities';
+} from "react-leaflet";
+import { GoogleLayer } from "react-leaflet-google";
+import MarkerClusterGroup from "react-leaflet-markercluster";
+import MapLegendControl from "./MapLegendControl";
+import Icon from "../Icon";
+import { IM_LIST2 } from "../iconConstants";
+import { convertPolygonsToLatLngArrays } from "../../utilities/mapUtilities";
 
 // using open street map for now because that way can use the clusters, etc.
 
 const GET_MUNICIPALITIES = gql`
-  query municipalities ($jurisdictions: [String]) {
-    municipalities (jurisdictions: $jurisdictions) {
-    jurisdiction
-    polygons {
-      outer {
-        points {
-          x
-          y
+  query municipalities($jurisdictions: [String]) {
+    municipalities(jurisdictions: $jurisdictions) {
+      jurisdiction
+      polygons {
+        outer {
+          points {
+            x
+            y
+          }
         }
-      }
-      holes {
-        points {
-          x
-          y
+        holes {
+          points {
+            x
+            y
+          }
         }
       }
     }
   }
-}
 `;
 
-const mapKey = 'AIzaSyAO8R1pXvBhpRoTFJ4d81MA8D8QBD0mPe0'; // restrict your referrers in cloud console
+const mapKey = "AIzaSyAO8R1pXvBhpRoTFJ4d81MA8D8QBD0mPe0"; // restrict your referrers in cloud console
 const { BaseLayer } = LayersControl;
-const satellite = 'SATELLITE';
+const satellite = "SATELLITE";
 
 function markerClusterKeyDown(e) {
-  if (e.originalEvent.key ===  'Enter') {
+  if (e.originalEvent.key === "Enter") {
     e.originalEvent.target.click();
   }
 }
@@ -57,15 +57,14 @@ function markerClusterKeyDown(e) {
 const createClusterCustomIcon = function (cluster) {
   return L.divIcon({
     html: `<div><span aria-label="${cluster.getChildCount()} projects">${cluster.getChildCount()}</span></div>`,
-    className: 'marker-cluster-custom marker-cluster marker-cluster-small',
+    className: "marker-cluster-custom marker-cluster marker-cluster-small",
     iconSize: L.point(40, 40, true),
   });
-}
+};
 const markerClusterOptions = {
   maxClusterRadius: 20,
   iconCreateFunction: createClusterCustomIcon,
 };
-
 
 // We can just pass undefined instead
 // const getBounds = (center, within) => {
@@ -88,9 +87,9 @@ const Map = (props) => {
 
   let shouldZoomToNonCenter = false;
   let zoomTo = null;
-  if (props.zoomToPoint !== null && props.zoomToPoint !== '') {
+  if (props.zoomToPoint !== null && props.zoomToPoint !== "") {
     shouldZoomToNonCenter = true;
-    zoomTo = props.zoomToPoint.split(',');
+    zoomTo = props.zoomToPoint.split(",");
     zoomTo = [parseFloat(zoomTo[0]), parseFloat(zoomTo[1])];
   }
 
@@ -105,73 +104,73 @@ const Map = (props) => {
         // Might need to update keyboard event in the next version
         onKeyPress={markerClusterKeyDown}
       >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
 
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-
-        {props.drawCircle &&
-          <Circle center={props.center} radius={props.radius} fillOpacity={0.22} />
-        }
-        {shouldZoomToNonCenter &&
+        {props.drawCircle && (
+          <Circle
+            center={props.center}
+            radius={props.radius}
+            fillOpacity={0.22}
+          />
+        )}
+        {shouldZoomToNonCenter && (
           <Circle center={zoomTo} radius={15} fillOpacity={0.15} color="red" />
-        }
-        {props.showCenter &&
+        )}
+        {props.showCenter && (
           <Marker
             position={props.center}
             icon={L.icon({
-              iconUrl: require('../../images/marker-icon-2.png'),
+              iconUrl: require("../../images/marker-icon-2.png"),
               iconSize: [25, 41],
               iconAnchor: [12, 41],
               popupAnchor: [2, -22],
             })}
           >
             <Popup>
-              <div><b>{props.centerLabel}</b></div>
+              <div>
+                <b>{props.centerLabel}</b>
+              </div>
             </Popup>
           </Marker>
-        }
+        )}
         {props.drawStreet &&
           props.streetData.map((line, index) => (
             <Polyline
-              key={['street_line', index].join('_')}
+              key={["street_line", index].join("_")}
               positions={line}
               weight={5}
               className="noPointer"
             />
-          ))
-        }
+          ))}
         {props.drawMaintenance &&
           props.maintenanceData.map((line, index) => (
             <Polyline
-              key={['maintenance_line', index].join('_')}
+              key={["maintenance_line", index].join("_")}
               positions={line.line}
               weight={10}
               opacity={0.8}
               color={line.color}
             >
-              <Popup>
-                {line.popup}
-              </Popup>
+              <Popup>{line.popup}</Popup>
             </Polyline>
-          ))
-        }
+          ))}
         {props.drawPolygon &&
           props.polygonData.map((poly, index) => (
             <Polygon
-              key={['polygon', index].join('_')}
+              key={["polygon", index].join("_")}
               positions={poly.polygons}
-              weight={1.5}
+              color={props.color}
+              opacity={props.opacity}
+              weight={props.weight}
+              fillColor={props.fillColor}
+              fillOpacity={props.fillOpacity}
             >
-              {poly.popup &&
-                <Popup>
-                  {poly.popup}
-                </Popup>
-              }
+              {poly.popup && <Popup>{poly.popup}</Popup>}
             </Polygon>
-          ))
-        }
+          ))}
         <Query
           query={GET_MUNICIPALITIES}
           variables={{
@@ -181,38 +180,42 @@ const Map = (props) => {
           {({ loading, error, data }) => {
             if (loading) return null;
             if (error) return null;
-            return (
-              data.municipalities.map(municipality => (
-                <Polygon
-                  key={municipality.jurisdiction}
-                  fillColor="#BEE8FF"
-                  fillOpacity={0.27}
-                  color="gray"
-                  weight="1.5"
-                  positions={convertPolygonsToLatLngArrays(municipality.polygons)}
-                  className="noPointer"
-                >
-                </Polygon>
-              ))
-            );
+            return data.municipalities.map((municipality) => (
+              <Polygon
+                key={municipality.jurisdiction}
+                fillColor="#BEE8FF"
+                fillOpacity={0.27}
+                color="gray"
+                weight="1.5"
+                positions={convertPolygonsToLatLngArrays(municipality.polygons)}
+                className="noPointer"
+              ></Polygon>
+            ));
           }}
         </Query>
         <MarkerClusterGroup markers={markers} {...markerClusterOptions} />
 
-        {
-          props.legend &&
+        {props.legend && (
           <MapLegendControl>
-            <div className="legendIcon"><Icon path={IM_LIST2} size={24} color="#828282" /></div>
+            <div className="legendIcon">
+              <Icon path={IM_LIST2} size={24} color="#828282" />
+            </div>
             <div
               className="legend"
-              style={{ maxHeight: parseInt(props.height.split('px')[0], 10) / 2 }}
+              style={{
+                maxHeight: parseInt(props.height.split("px")[0], 10) / 2,
+              }}
             >
               <div
                 className="closeLegend"
-                style={{ fontWeight: 'bold', fontSize: '14px', color: '#979797' }}
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "14px",
+                  color: "#979797",
+                }}
                 onClick={(e) => {
-                  if (e.target.parentNode.style.display === 'block') {
-                    e.target.parentNode.style.display = 'none';
+                  if (e.target.parentNode.style.display === "block") {
+                    e.target.parentNode.style.display = "none";
                   }
                 }}
               >
@@ -221,7 +224,7 @@ const Map = (props) => {
               {props.legend}
             </div>
           </MapLegendControl>
-        }
+        )}
       </LeafletMap>
     </div>
   );
@@ -233,6 +236,11 @@ Map.propTypes = {
   name: PropTypes.string,
   width: PropTypes.string,
   height: PropTypes.string,
+  fillColor: PropTypes.string,
+  fillOpacity: PropTypes.number,
+  color: PropTypes.string,
+  opacity: PropTypes.number,
+  weight: PropTypes.number,
   drawCircle: PropTypes.bool,
   drawPolygon: PropTypes.bool,
   radius: PropTypes.number,
@@ -255,22 +263,27 @@ Map.defaultProps = {
   center: [35.5951, -82.5515],
   centerLabel: null,
   data: [],
-  height: '600px',
+  height: "600px",
   drawCircle: false,
   drawStreet: false,
   drawPolygon: false,
   drawMaintenance: false,
   maintenanceData: null,
-  municipalities: ['Asheville Corporate Limits'],
-  name: '',
+  municipalities: ["Asheville Corporate Limits"],
+  name: "",
   polygonData: null,
   radius: 83,
   showCenter: false,
   streetData: null,
-  width: '100%',
+  width: "100%",
   within: 1320,
   zoomToPoint: null,
   zoom: 16,
+  fillColor: "#58a2ff",
+  fillOpacity: 0.35,
+  color: "#004987",
+  opacity: 1,
+  weight: 1.5,
 };
 
 export default Map;

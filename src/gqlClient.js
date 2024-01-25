1a4d63a710944218bce69b7043fd7fdbc345b316
Jon Wilson <jontwilson@gmail.com>
2024-01-25 13:17:31 -0500

    change dev server url

--- src/gqlClient.js
+++ src/gqlClient.js
@@ -10,8 +10,8 @@ import { fragmentTypes } from './fragmentTypes';
 
 let SERVER_URL = 'https://data-api1.ashevillenc.gov/graphql';
 if (process.env.REACT_APP_USE_DEV_API === true || process.env.REACT_APP_USE_DEV_API === 'true') {
-  // SERVER_URL = 'https://rdayudbrdjqujgfoauqrf42r7a0vzpai.lambda-url.us-east-1.on.aws/';
-  SERVER_URL = 'https://dev-data-api2.ashevillenc.gov/graphql';
+  SERVER_URL = 'https://rdayudbrdjqujgfoauqrf42r7a0vzpai.lambda-url.us-east-1.on.aws/';
+  // SERVER_URL = 'https://dev-data-api2.ashevillenc.gov/graphql';
 }
 if (process.env.REACT_APP_USE_LOCAL_API === true || process.env.REACT_APP_USE_LOCAL_API === 'true') {
   SERVER_URL = 'http://localhost:8080/graphql';

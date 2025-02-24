f739da9dc6eb4346a64be4aef175ea28361bed4a
6foot5 <6foot5@gmail.com>
2025-02-24 09:39:28 -0500

    add GIS maintenance notice

--- src/app/App.js
+++ src/app/App.js
@@ -80,13 +80,14 @@ class Main extends React.Component {
           {displayNavbar(this.props.location.query.hideNavbar)}
           <div className="container" id="content">
             <EnvBanner />
-            {/* <Banner color="orange" path="/">
+            <Banner color="orange" path="*">
               <>
-                <p style={{fontWeight: '500', fontSize: '1.5rem', textAlign: 'center'}}><em>Notice: Intermittent Search Errors</em></p>
-                <p>We are aware of intermittent errors when searching for addresses in SimpliCity and are working on a fix. 
-                  If your search produces an error, please wait a moment and try agian.</p>
+                <p style={{fontWeight: '500', fontSize: '1.25rem', textAlign: 'center' }}>
+                  <em>IMPORTANT: Planned Maintenance Outage for All GIS Services</em>
+                </p>
+                <p style={{textAlign: 'center', fontSize: '1.25rem', }}>Friday, February 28, 7 a.m. – 10 a.m.</p>
               </>
-            </Banner> */}
+            </Banner>
             <ErrorBoundary>{this.props.children}</ErrorBoundary>
           </div>
           {!this.props.location.query.hideNavbar && <Footer />}

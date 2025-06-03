cbb448e79d82130927c9fcdd7cf64cf55e5ba591
CameronHenshaw <49311293+CameronHenshaw@users.noreply.github.com>
2025-06-03 09:28:10 -0400

    tiny change to make purple markers stand out

--- src/app/capital_projects/CIPColors.js
+++ src/app/capital_projects/CIPColors.js
@@ -3,6 +3,6 @@ export const CIPcolors = {
     "Bond": "#2A579A",
     "Operating Budget": "#E67125",
     "CIP": "#E67125",
-    "Bond 2024": "#762cb2",
+    "Bond 2024": "#9c2cb2",
     Helene: "#A21E1E",
   };

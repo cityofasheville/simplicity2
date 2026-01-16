d5aa770aa6512b308d6f98281ff1bc6e542c6ad4
6foot5 <6foot5@gmail.com>
2026-01-16 15:37:14 -0500

    standardize custom tw color names

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,js,jsx}'],
-    theme: {
-      extend: {
-        colors: {
-          transparent: 'transparent',
-          coa: {
-            darkBlue: '#004987',
-            medBlue: '#4077a5',
-            lightBlue: '#a6bfd5',
-  
-            darkGreen: '#aaad00',
-            medGreen: '#bfc240',
-            lightGreen: '#e1e2a6',
-          },
-  
-          gray: {
-            dark: '#222222',
-            light: '#b4bcc2',
-            lighter: '#ecf0f1',
-          },
-  
-          brand: {
-            primary: '#4077a5',
-            secondary: '#335f84',
-            tertiary: '#4986b8',
-            success: '#11866f',
-            info: '#3498db',
-            warning: '#f39c12',
-            danger: '#e74c3c',
-          },
-        },
+  content: ['./src/**/*.{html,js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        primary: '#4077a5',
+        secondary: '#335f84',
+        tertiary: '#4986b8',
+        success: '#11866f',
+        info: '#3498db',
+        warning: '#f39c12',
+        danger: '#e74c3c',
+        'coa-blue-dark': '#004987',
+        'coa-blue-medium': '#4077a5',
+        'coa-blue-light': '#a6bfd5',
+        'coa-green-dark': '#aaad00',
+        'coa-green-medium': '#bfc240',
+        'coa-green-light': '#e1e2a6',
       },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};

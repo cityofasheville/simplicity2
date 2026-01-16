705ad7854ca0901dd999466e88810f02e3fab0e8
CameronHenshaw <49311293+CameronHenshaw@users.noreply.github.com>
2026-01-16 11:56:20 -0500

    tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js,jsx}'],
+    theme: {
+      extend: {
+        colors: {
+          transparent: 'transparent',
+        },
+      },
+    },
+    plugins: [],
+  };

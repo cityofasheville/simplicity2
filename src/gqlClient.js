e623511c0bbd445df05a080eb8de8a113c213d96
CameronHenshaw <49311293+CameronHenshaw@users.noreply.github.com>
2026-09-24 15:38:35 -0400

    api url update

--- src/gqlClient.js
+++ src/gqlClient.js
@@ -16,7 +16,8 @@ if (process.env.REACT_APP_USE_DEV_API === true || process.env.REACT_APP_USE_DEV_
 if (process.env.REACT_APP_USE_LOCAL_API === true || process.env.REACT_APP_USE_LOCAL_API === "true") {
 	SERVER_URL = "http://localhost:8080/graphql";
 }
-let SERVER_URL = "https://climate-data-api2.ashevillenc.gov/graphql";
+// let SERVER_URL = "https://climate-data-api2.ashevillenc.gov/graphql";
+let SERVER_URL = "https://8nj7nwqytg.execute-api.us-east-1.amazonaws.com";
 
 const httpLink = createHttpLink({ uri: SERVER_URL, fetch });
 
